a8d90332f9e1fd2e30148c218eef166b5bc91bdd
cying111 <chen_ying@gis.a-star.edu.sg>
2020-02-11 16:24:14 +0800

    Adding .sqlite file
    Former-commit-id: ce4e99425189d25fd34fca48b3d2eb58932bf93b

--- R/chenInputParam.R
+++ R/chenInputParam.R
@@ -10,14 +10,14 @@ checkInput <- function(input.list){
   default.algo.control <- list(ncore = detectCores(),
                                method = "two-step",
                                convcontrol = 10^(-3))
-}
+
 if(is.null(algo.control)){
   algo.control <- default.algo.control
 }else{
   if(is.null(algo.control[["ncore"]])|(as.numeric(algo.control[["ncore"]])>default.algo.control[["ncore"]])){
     algo.control[["ncore"]] <- default.algo.control[["ncore"]]
   }
-  if(is.null(algo.control[["method"]])|(!(algo.control[["method"]] %in% c("one-step","two-step"))){
+  if(is.null(algo.control[["method"]])|(!(algo.control[["method"]] %in% c("one-step","two-step")))){
     algo.control[["method"]] <- default.algo.control[["method"]]
   }
   if(is.null(algo.control[["method"]])){
