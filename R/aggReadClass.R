3aa124ee9e9ec8514ec9f969af243872035d379a
cying111 <chen_ying@gis.a-star.edu.sg>
2020-02-13 10:36:53 +0800

    Modify bamboo.R to fix bugs
    Former-commit-id: ae13bb02f898beca988d68827051a03e72695a64

--- R/aggReadClass.R
+++ R/aggReadClass.R
@@ -3,7 +3,7 @@
 #'@param mapping
 #'@param nobs
 aggReadClass <- function(dt){
-  dt[, tx_p:=paste(sort(unique(tx_sid)), collapse = '.'), by = list(read_class_sid,gene_sid)]
+  dt[, equiClass_p:=paste(sort(unique(tx_sid)), collapse = '.'), by = list(read_class_sid,gene_sid)]
   dt[, read_class_sid_stored:=read_class_sid]
   equiClassVec <- unique(dt$equiClass_p)
   dt[, read_class_sid:=match(equiClass_p,equiClassVec)]
@@ -11,7 +11,7 @@ aggReadClass <- function(dt){
   dt[, nobs:=NULL]
   tmp <- unique(dt[,.(read_class_sid, read_class_sid_stored, nobs_stored)])
   tmp[, nobs:=sum(nobs_stored), by = read_class_sid]
-  tmp <- unique(tmp[,.(read_class_id, nobs)])
+  tmp <- unique(tmp[,.(read_class_sid, nobs)])
   dt <- unique(dt[,.(read_class_sid, tx_sid,gene_sid)])[tmp, on = 'read_class_sid']
   return(list(dt,equiClassVec))
 }
