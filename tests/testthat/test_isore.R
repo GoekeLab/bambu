28e1416cb627a34e8c5b02997fa03e27371716df
rstudio <rstudio@ip-10-0-1-203.ap-southeast-1.compute.internal>
2020-02-15 08:09:05 +0000

    Add unit test for prepareAnnotations, isore and bamboo.R
    Former-commit-id: db09f2102bda9d817b5bd8c3205af4c73d7ea1f4

--- tests/testthat/test_isore.R
+++ tests/testthat/test_isore.R
@@ -1,33 +1,22 @@
 context("Isoform reconstruction")
 library(bamboo)
 
-test_that("Prepare annotations of txdb is List",{
-  samplefile <- system.file("extdata","Homo_sapiens.GRCh38.91.annotations-txdb.sqlite", package = "bamboo")
-  txdb <- AnnotationDbi::loadDb(samplefile)
-  expect_output(prepareAnnotations(txdb), "test that rows are ordered by tx name")
-  expect_type(prepareAnnotations(txdb),"list")
-  expect_named(prepareAnnotations(txdb), c("intronsByTxEns" ,"exonsByTx","txIdToGeneIdTable","unlisted_introns" ))
-
-})
 
 
 test_that("isoform reconstruction of bamFile is summarizedExperiment", {
-  samplefile <- system.file("extdata","Homo_sapiens.GRCh38.91.annotations-txdb.sqlite", package = "bamboo")
-  txdb <- AnnotationDbi::loadDb(samplefile)
   test.bam <- system.file("extdata", "GIS_HepG2_cDNAStranded_Rep5-Run4_chr9_108865774_109014097.bam", package = "bamboo")
   fa.file <- system.file("extdata", "Homo_sapiens.GRCh38.dna_sm.primary_assembly_chr9.fa.gz", package = "bamboo")
-  expect_output(isore(bamFile = test.bam,  txdb = txdb,genomeFA = fa.file), "### prepare annotations ###")
-  expect_s4_class(isore(bamFile = test.bam,  txdb = txdb,genomeFA = fa.file), "summarizedExperiment")
-  expect_equal(isore(bamFile = test.bam,  txdb = txdb,genomeFA = fa.file), sysdata_se)
+  expect_output(isore(bamFile = test.bam,  txdbTablesList = example_txdbTablesList,genomeFA = fa.file), "### prepare annotations ###")
+  expect_s4_class(isore(bamFile = test.bam,  txdbTablesList = example_txdbTablesList,genomeFA = fa.file), "SummarizedExperiment")
+  expect_equal(isore(bamFile = test.bam,  txdbTablesList = example_txdbTablesList,genomeFA = fa.file), example_se)
 })
 
 
 test_that("isoform reconstruction of missing bam file is error", {
-  samplefile <- system.file("extdata","Homo_sapiens.GRCh38.91.annotations-txdb.sqlite", package = "bamboo")
-  txdb <- AnnotationDbi::loadDb(samplefile)
+
   test.bam <- system.file("extdata", "GIS_HepG2_cDNAStranded_Rep5-Run4_chr9_108865774_109014097.bam", package = "bamboo")
   fa.file <- system.file("extdata", "Homo_sapiens.GRCh38.dna_sm.primary_assembly_chr9.fa.gz", package = "bamboo")
-  expect_error(isore(bamFile = '',  txdb = txdb,genomeFA = fa.file),"Bam file is missing from arguments.")
+  expect_error(isore(bamFile = '',  txdbTablesList = example_txdbTablesList,genomeFA = fa.file),"Bam file is missing from arguments.")
 })
 
 test_that("isoform reconstruction of missing both txdb and txdbTablesList object is error", {
@@ -38,9 +27,7 @@ test_that("isoform reconstruction of missing both txdb and txdbTablesList object
 })
 
 test_that("isoform reconstruction of missing fa file is error", {
-  samplefile <- system.file("extdata","Homo_sapiens.GRCh38.91.annotations-txdb.sqlite", package = "bamboo")
-  txdb <- AnnotationDbi::loadDb(samplefile)
   test.bam <- system.file("extdata", "GIS_HepG2_cDNAStranded_Rep5-Run4_chr9_108865774_109014097.bam", package = "bamboo")
-  expect_error(isore(bamFile = test.bam,  txdbTablesList = txdb,genomeFA = ""))
-  expect_error(isore(bamFile = test.bam,  txdbTablesList = txdb,genomeFA = NULL),"GenomeFA file is missing.")
+  expect_error(isore(bamFile = test.bam,  txdbTablesList = example_txdbTablesList,genomeFA = ""))
+  expect_error(isore(bamFile = test.bam,  txdbTablesList = example_txdbTablesList,genomeFA = NULL),"GenomeFA file is missing.")
 })
