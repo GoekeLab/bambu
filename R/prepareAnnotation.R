c08e89c0726306d7404487be4a796ccce5444edd
Jonathan Goeke <10560051+jonathangoeke@users.noreply.github.com>
2020-03-19 17:09:44 +0800

    added function to prepare annotation granges lsit (smaller than the old txdbtableList)

--- R/prepareAnnotation.R
+++ R/prepareAnnotation.R
@@ -8,7 +8,7 @@
 #'  txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
 #'  prepareAnnotations(txdb)
 #'  }
-prepareAnnotations <- function(txdb) {
+prepareAnnotationsAsList <- function(txdb) {
   txdbTablesList <- list()
   txdbTablesList[['intronsByTxEns']] <- intronsByTranscript(txdb,use.names=TRUE)
 
@@ -77,6 +77,47 @@ prepareAnnotations <- function(txdb) {
 
   return(txdbTablesList)
 }
+
+prepareAnnotations <- function(txdb) {
+  exonsByTx = exonsBy(txdb,by='tx', use.names=TRUE)
+  unlistedExons <- unlist(exonsByTx, use.names = FALSE)
+  partitioning <- PartitioningByEnd(cumsum(elementNROWS(exonsByTx)), names=NULL)
+  txIdForReorder <- togroup(PartitioningByWidth(exonsByTx))
+  unlistedExons <- unlistedExons[order(txIdForReorder, unlistedExons$exon_rank)]  # txdbTablesList[['exonsByTx']] is always sorted by exon rank, not by strand, make sure that this is the case here
+  unlistedExons$exon_endRank <- unlist(sapply(elementNROWS(exonsByTx),seq,to=1))
+  unlistedExons <- unlistedExons[order(txIdForReorder, start(unlistedExons))]
+  mcols(unlistedExons) <- mcols(unlistedExons)[,c('exon_rank','exon_endRank')]
+  exonsByTx <- relist(unlistedExons, partitioning)
+
+  mcols(exonsByTx) <-  AnnotationDbi::select(txdb, names(exonsByTx),
+                        columns=c("TXNAME", "GENEID"),
+                        keytype="TXNAME")
+  minEqClasses <- getMinimumEqClassByTx(exonsByTx)
+  mcols(exonsByTx)$eqClass <- minEqClasses$eqClass[match(names(exonsByTx),minEqClasses$queryTxId)]
+  return(exonsByTx)
+}
+
+
+getMinimumEqClassByTx <- function(exonsByTranscripts) {
+
+  exByTxAnnotated_singleBpStartEnd <- cutStartEndFromGrangesList(exonsByTranscripts)  # estimate overlap only based on junctions
+  spliceOverlaps=findSpliceOverlapsQuick(exByTxAnnotated_singleBpStartEnd,exByTxAnnotated_singleBpStartEnd)  ## identify transcripts which are compatbile with other transcripts (subsets by splice sites)
+  spliceOverlapsSelected =spliceOverlaps[mcols(spliceOverlaps)$compatible==TRUE,] ## select splicing compatible transcript matches
+
+  minReadClassTable <- as_tibble(spliceOverlapsSelected) %>%
+    dplyr::select(queryHits, subjectHits)
+  minReadClassTable$queryTxId <- names(exByTxAnnotated_singleBpStartEnd)[minReadClassTable$queryHits]
+  minReadClassTable$subjectTxId <- names(exByTxAnnotated_singleBpStartEnd)[minReadClassTable$subjectHits]
+  minReadClassTable <- minReadClassTable %>%
+    group_by(queryTxId) %>%
+    arrange(queryTxId, subjectTxId) %>%
+    mutate(eqClass = paste(subjectTxId, collapse='.'), minEqClassSize = n()) %>%
+    dplyr::select(queryTxId, eqClass, minEqClassSize) %>%
+    distinct()
+  return(minReadClassTable)
+}
+
+
 
 
 
