2010525b56f5e1499ec3efd82503a9e696af1c30
Jonathan Goeke <10560051+jonathangoeke@users.noreply.github.com>
2020-03-19 17:57:53 +0800

    updated function to work with grangesList instead of txdbTableList
    Former-commit-id: 969de090df81ab1c2f61ee2ac763c3d81feea234

--- R/getEmptyClassFromSE.R
+++ R/getEmptyClassFromSE.R
@@ -1,11 +1,13 @@
-getEmptyClassFromSE <- function(se = se, txdbTablesList = NULL){
+
+
+getEmptyClassFromSE <- function(se = se, annotationGrangesList = NULL){
   distTable <- data.table(metadata(se)$distTable)[,.(readClassId, annotationTxId, readCount, GENEID)]
   distTable[, eqClass:=paste(sort(unique(annotationTxId)),collapse='.'), by = list(readClassId,GENEID)]
   distTable[,eqClassReadCount:=sum(readCount), by = list(eqClass, GENEID)]
   eqClassCountTable <- unique(distTable[,.(annotationTxId, GENEID, eqClass, eqClassReadCount)])
   #eqClassCountTable[,list(readCount=sum(readCount)), by = list(eqClass, GENEID)]
   setnames(eqClassCountTable, c("annotationTxId"),c("TXNAME"))
-  eqClassTable <- data.table(txdbTablesList$txIdToGeneIdTable)
+  eqClassTable <- as.data.table(mcols(annotationGrangesList)[,c('GENEID','eqClass','TXNAME')])
   #eqClassCountTable <- eqClassCountTable[, list(TXNAME=unlist(strsplit(eqClass,'\\.'))), by = list(eqClass,GENEID, readCount)]
   #check that if observed classes is more than reference eqClasses
 
@@ -24,29 +26,57 @@ getEmptyClassFromSE <- function(se = se, txdbTablesList = NULL){
   return(eqClassCountTable)
 
 }
-
-
-getEmptyClassFromSEAndRanges <- function(se = se, annotationGrangesList = NULL){
-  distTable <- data.table(metadata(se)$distTable)[,.(readClassId, annotationTxId, readCount, GENEID)]
-  distTable[, eqClass:=paste(sort(unique(annotationTxId)),collapse='.'), by = list(readClassId,GENEID)]
-
-  eqClassCountTable <- distTable[,list(readCount=sum(readCount)), by = list(eqClass, GENEID)]
-  eqClassTable <- as.data.table(mcols(annotationGrangesList))
-  eqClassCountTable <- eqClassCountTable[, list(TXNAME=unlist(strsplit(eqClass,'\\.'))), by = list(eqClass,GENEID, readCount)]
-  #check that if observed classes is more than reference eqClasses
-
-  eqClassCountTable <- unique(merge(eqClassCountTable,eqClassTable,all = TRUE, on = c('GENEID','eqClass','TXNAME'))) # merge should be performed on both sides
-  # if there exists new isoforms from eqClassCountTable, it would not be found in eqClassTable, keep them
-  eqClassCountTable[is.na(readCount), readCount:=0]
-
-  ## remove empty read class where there is no shared class found
-  eqClassCountTable[,sum_nobs:=sum(readCount), by = list(GENEID, TXNAME)]
-
-  eqClassCountTable <- unique(eqClassCountTable[sum_nobs>0,.(GENEID, eqClass, readCount,TXNAME)])
-
-  setnames(eqClassCountTable,old = c("TXNAME","GENEID","eqClass","readCount") , new = c("tx_id","gene_id","read_class_id","nobs"))
-
-
-  return(eqClassCountTable)
-
-}
+#
+#
+#
+# getEmptyClassFromSERanges <- function(se = se, annotationGrangesList = NULL){
+#   distTable <- data.table(metadata(se)$distTable)[,.(readClassId, annotationTxId, readCount, GENEID)]
+#   distTable[, eqClass:=paste(sort(unique(annotationTxId)),collapse='.'), by = list(readClassId,GENEID)]
+#
+#   eqClassCountTable <- distTable[,list(readCount=sum(readCount)), by = list(eqClass, GENEID)]
+#   eqClassTable <- as.data.table(mcols(annotationGrangesList))
+#   eqClassCountTable <- eqClassCountTable[, list(TXNAME=unlist(strsplit(eqClass,'\\.'))), by = list(eqClass,GENEID, readCount)]
+#   #check that if observed classes is more than reference eqClasses
+#
+#   eqClassCountTable <- unique(merge(eqClassCountTable,eqClassTable,all = TRUE, on = c('GENEID','eqClass','TXNAME'))) # merge should be performed on both sides
+#   # if there exists new isoforms from eqClassCountTable, it would not be found in eqClassTable, keep them
+#   eqClassCountTable[is.na(readCount), readCount:=0]
+#
+#   ## remove empty read class where there is no shared class found
+#   eqClassCountTable[,sum_nobs:=sum(readCount), by = list(GENEID, TXNAME)]
+#
+#   eqClassCountTable <- unique(eqClassCountTable[sum_nobs>0,.(GENEID, eqClass, readCount,TXNAME)])
+#
+#   setnames(eqClassCountTable,old = c("TXNAME","GENEID","eqClass","readCount") , new = c("tx_id","gene_id","read_class_id","nobs"))
+#
+#
+#   return(eqClassCountTable)
+#
+# }
+#
+# getEmptyClassFromSEOld <- function(se = se, txdbTablesList = NULL){
+#   distTable <- data.table(metadata(se)$distTable)[,.(readClassId, annotationTxId, readCount, GENEID)]
+#   distTable[, eqClass:=paste(sort(unique(annotationTxId)),collapse='.'), by = list(readClassId,GENEID)]
+#   distTable[,eqClassReadCount:=sum(readCount), by = list(eqClass, GENEID)]
+#   eqClassCountTable <- unique(distTable[,.(annotationTxId, GENEID, eqClass, eqClassReadCount)])
+#   #eqClassCountTable[,list(readCount=sum(readCount)), by = list(eqClass, GENEID)]
+#   setnames(eqClassCountTable, c("annotationTxId"),c("TXNAME"))
+#   eqClassTable <- data.table(txdbTablesList$txIdToGeneIdTable)
+#   #eqClassCountTable <- eqClassCountTable[, list(TXNAME=unlist(strsplit(eqClass,'\\.'))), by = list(eqClass,GENEID, readCount)]
+#   #check that if observed classes is more than reference eqClasses
+#
+#   eqClassCountTable <- unique(merge(eqClassCountTable,eqClassTable,all = TRUE, on = c('GENEID','eqClass','TXNAME'))) # merge should be performed on both sides
+#   # if there exists new isoforms from eqClassCountTable, it would not be found in eqClassTable, keep them
+#   eqClassCountTable[is.na(eqClassReadCount), eqClassReadCount:=0]
+#
+#   ## remove empty read class where there is no shared class found
+#   eqClassCountTable[,sum_nobs:=sum(eqClassReadCount), by = list(GENEID, TXNAME)]
+#
+#   eqClassCountTable <- unique(eqClassCountTable[sum_nobs>0,.(GENEID, eqClass, eqClassReadCount,TXNAME)])
+#
+#   setnames(eqClassCountTable,old = c("TXNAME","GENEID","eqClass","eqClassReadCount") , new = c("tx_id","gene_id","read_class_id","nobs"))
+#
+#
+#   return(eqClassCountTable)
+#
+# }
