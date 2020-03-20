1aaf2b425b12d728f5c331430d86b695fe54de5d
Jonathan Goeke <10560051+jonathangoeke@users.noreply.github.com>
2020-03-20 15:29:48 +0800

    added test run for bamboo with isoform discovery on single and multiple samples
    Former-commit-id: 566b49663442b73e7b692be4c0c2b07786869995

--- tests/testthat/test_bamboo.R
+++ tests/testthat/test_bamboo.R
@@ -26,31 +26,34 @@ test_that("isoform quantification of summarizedExperiment is summrizedExperiment
 })
 
 test_that("bamboo.quantISORE (isoform quantification of bam file) produces expected output",{
-
-  ## ToDo: update data sets for comparison
+   ## ToDo: update data sets for comparison
   test.bam <- system.file("extdata", "GIS_HepG2_cDNAStranded_Rep5-Run4_chr9_108865774_109014097.bam", package = "bamboo")
   fa.file <- system.file("extdata", "Homo_sapiens.GRCh38.dna_sm.primary_assembly_chr9.fa.gz", package = "bamboo")
-  example_txdbTablesList <- readRDS(url("http://s3.ap-southeast-1.amazonaws.com/ucsc-trackdata.store.genome.sg/chenying/bamboo_exampleDataset/HomoSapiens_ensembl91_Grch38_txdbTablesList.rds"))
+  gr <- prepareAnnotations(txdb) ## saveRDS(prepareAnnotations(txdb), file='annotationGranges.rds', compress = 'xz')
+  stranded=FALSE
 
-  example_quantOutput <- readRDS(url("http://s3.ap-southeast-1.amazonaws.com/ucsc-trackdata.store.genome.sg/chenying/bamboo_exampleDataset/example_quantOutput.rds")) ## ToDo: should be updated
+  # needs to be updated
+  #example_quantOutput <- readRDS(url("http://s3.ap-southeast-1.amazonaws.com/ucsc-trackdata.store.genome.sg/chenying/bamboo_exampleDataset/example_quantOutput.rds")) ## ToDo: should be updated
 
   # test case 1: bamboo with single bam file, only using annotations (default option)
-  se = bamboo(bam.file = test.bam, fa.file = fa.file, txdbTablesList = example_txdbTablesList, algo.control=list(ncore = 1))
-  se.isore = bamboo.quantISORE(bam.file = test.bam, fa.file = fa.file, txdbTablesList = example_txdbTablesList, algo.control=list(ncore = 1))
+  se = bamboo(bam.file = test.bam,  txdb = txdb, fa.file = fa.file, algo.control=list(ncore = 1), extendAnnotations=F)
+  se = bamboo(bam.file = test.bam,  annotationGrangesList =  gr, fa.file = fa.file, algo.control=list(ncore = 1), extendAnnotations=F)
+  se = bamboo(bam.file = test.bam,  annotationGrangesList =  gr, fa.file = fa.file, algo.control=list(ncore = 1), extendAnnotations=T)
+
 
   expect_s4_class(se, "SummarizedExperiment")
-  expect_equal(assays(se),assays(example_quantOutput))
-  expect_equal(se,se.isore)
+ # expect_equal(assays(se),assays(example_quantOutput))
+ # expect_equal(se,se.isore)
 
   # test case 2: bamboo with multiple bam file, only using annotations (default option), yieldSize lower than read count
-  se = bamboo(bam.file = BamFileList(test.bam, test.bam, yieldSize = 1000), fa.file = fa.file, txdbTablesList = example_txdbTablesList, algo.control=list(ncore = 1))
+  se = bamboo(bam.file = BamFileList(c(test.bam, test.bam), yieldSize = 1000),  annotationGrangesList =  gr, fa.file = fa.file, algo.control=list(ncore = 1), extendAnnotations=FALSE)
   expect_s4_class(se, "SummarizedExperiment")
-  expect_equal(assays(se)[,1],assays(example_quantOutput))
+#  expect_equal(assays(se)[,1],assays(example_quantOutput))
 
   # test case 3: bamboo with multiple bam file, extending annotations, yieldSize lower than read count
-  se = bamboo(bam.file = BamFileList(test.bam, test.bam, yieldSize = 1000), fa.file = fa.file, txdbTablesList = example_txdbTablesList, algo.control=list(ncore = 1), extendAnnotations = TRUE)
+  se = bamboo(bam.file = BamFileList(c(test.bam, test.bam), yieldSize = 1000),  annotationGrangesList =  gr, fa.file = fa.file, algo.control=list(ncore = 1), extendAnnotations=TRUE)
   expect_s4_class(se, "SummarizedExperiment")
-  expect_equal(assays(se)[,1],assays(example_quantOutput))
+ # expect_equal(assays(se)[,1],assays(example_quantOutput))
 
 
 })
