407d80bbc3f1fad167253a41cb6136675d528e49
Jonathan Goeke <10560051+jonathangoeke@users.noreply.github.com>
2020-04-11 11:35:09 +0800

    updated test for prepare annotations
    Former-commit-id: 7d69a15a8e42a4939c4cbdac411107ae6d478e99

--- tests/testthat/test_prepareAnnotations.R
+++ tests/testthat/test_prepareAnnotations.R
@@ -8,9 +8,12 @@ test_that("Prepare annotations of txdb is List",{
   dbfile <- tempfile(fileext=".sqlite")
   download.file(samplefile, dbfile)
   txdb <- AnnotationDbi::loadDb(dbfile)
-  expect_output(prepareAnnotations(txdb), "test that rows are ordered by tx name")
-  expect_type(prepareAnnotations(txdb),"list")
-  expect_named(prepareAnnotations(txdb), c("intronsByTxEns" ,"exonsByTx","txIdToGeneIdTable","unlisted_introns" ))
-  example_txdbTablesList <- readRDS(url("http://s3.ap-southeast-1.amazonaws.com/ucsc-trackdata.store.genome.sg/chenying/bamboo_exampleDataset/HomoSapiens_ensembl91_Grch38_txdbTablesList.rds"))
-  expect_equal(prepareAnnotations(txdb), example_txdbTablesList)
+
+  expectedFile <- readRDS('/mnt/ont/github/testdata/testthat_bamboo/annotationGranges_txdbGrch38_91.rds')
+
+  gr <- prepareAnnotations(txdb)
+
+  expect_equal(gr, expectedFile)
+  expect_s4_class(gr, class = 'CompressedGRangesList')
+  expect_named(mcols(gr), c("TXNAME", "GENEID","eqClass"))
 })
