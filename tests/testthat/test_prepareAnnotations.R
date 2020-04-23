b72b0e8b2624964f7c8725df7f403fc04455a03d
cying111 <chen_ying@gis.a-star.edu.sg>
2020-04-23 11:22:14 +0000

    Amended unit test for prepareAnnotations and bamboo functions

--- tests/testthat/test_prepareAnnotations.R
+++ tests/testthat/test_prepareAnnotations.R
@@ -2,18 +2,44 @@ context("Prepare annotations")
 library(bamboo)
 library(RSQLite)
 
-test_that("Prepare annotations of txdb is List",{
+test_that("prepareAnnotations of txdb object is a GRangesList",{
   samplefile <- "http://s3.ap-southeast-1.amazonaws.com/ucsc-trackdata.store.genome.sg/chenying/bamboo_exampleDataset/Homo_sapiens.GRCh38.91.annotations-txdb.sqlite"
   # download remote sqlite file as a temporary object
   dbfile <- tempfile(fileext=".sqlite")
   download.file(samplefile, dbfile)
   txdb <- AnnotationDbi::loadDb(dbfile)
 
-  expectedFile <- readRDS('/mnt/ont/github/testdata/testthat_bamboo/annotationGranges_txdbGrch38_91.rds')
+
+  expectedGR <- readRDS(system.file("extdata", "annotationGrangesList_txdbGrch38_91.rds", package = "bamboo"))
+
 
   gr <- prepareAnnotations(txdb)
 
-  expect_equal(gr, expectedFile)
+  expect_equal(gr, expectedGR)
+  expect_s4_class(gr, class = 'CompressedGRangesList')
+  expect_named(mcols(gr), c("TXNAME", "GENEID","eqClass"))
+})
+
+
+test_that("prepareAnnotations of genome library is a GRangesList",{
+  library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+  gr <- prepareAnnotations(TxDb.Hsapiens.UCSC.hg38.knownGene)
+
+  expect_s4_class(gr, class = 'CompressedGRangesList')
+  expect_named(mcols(gr), c("TXNAME", "GENEID","eqClass"))
+})
+
+
+
+
+test_that("prepareAnnotationsFromGTF is GRangesList",{
+  gtf.file <- system.file("extdata", "Homo_sapiens.GRCh38.91_chr9.gtf", package = "bamboo")
+
+  expectedGR <- readRDS(system.file("extdata", "annotationGranges_txdbGrch38_91_chr9.rds", package = "bamboo"))
+
+  gr <- prepareAnnotationsFromGTF(gtf.file)
+
+  expect_equal(gr, expectedGR)
   expect_s4_class(gr, class = 'CompressedGRangesList')
   expect_named(mcols(gr), c("TXNAME", "GENEID","eqClass"))
 })
