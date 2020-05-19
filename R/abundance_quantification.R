66ac6c3d65059833941bc74981efed9b65deaa5a
cying111 <chen_ying@gis.a-star.edu.sg>
2020-05-19 11:36:20 +0000

    Fixed bugs for parallel em computation
    Former-commit-id: 3941f1e1eaeeeadeaf22597b35bf5ad47e7229fa

--- R/abundance_quantification.R
+++ R/abundance_quantification.R
@@ -3,37 +3,27 @@
 #' @param method A string variable indicates the whether a one-step or two-step approach will be used. See \code{Details}
 #' for details on one-step and two-step approach.
 #' @param read_classDT A \code{data.table} with columns
+#' @importFrom BiocParallel bplapply
 #' @noRd
 abundance_quantification <- function(read_classDT,mc.cores = 1,
                                      bias_correction = TRUE,
                                      maxiter = 20000,
                                      conv.control = 10^(-8)){
   gene_sidList <- unique(read_classDT$gene_sid)
-  n_gene <- length(gene_sidList)
-  n_chunk <- ceiling(n_gene/2000)
-  splitIndex <- split(1:n_gene, rep(1:n_chunk, ceiling(n_gene/n_chunk))[1:n_gene])
-  pb <- progress::progress_bar$new(
-    format = "  Genes [:bar] :percent in :elapsed",
-    total = n_chunk, clear = FALSE, width= 60)
-  pb$tick(0)
 
-  outData <- lapply(as.list(1:n_chunk), function(i){
-    gene_sid_chunk <- gene_sidList[splitIndex[[i]]]
-    outData <- parallel::mclapply(gene_sid_chunk,run_parallel,
-                                  conv.control = conv.control,
-                                  bias_correction = bias_correction,
-                                  maxiter = maxiter, read_classDT = read_classDT,
-                                  mc.set.seed = TRUE,mc.preschedule = TRUE,
-                                  mc.silent = FALSE, mc.cores = mc.cores)
+  bpParameters <- BiocParallel::bpparam()
+  bpParameters$workers <- mc.cores
+  emResultsList <- BiocParallel::bplapply(as.list(gene_sidList),
+                                          run_parallel,
+                                          conv.control = conv.control,
+                                          bias_correction = bias_correction,
+                                          maxiter = maxiter,
+                                          read_classDT = read_classDT,
+                                          BPPARAM=bpParameters)
 
-    outData <- list(do.call('rbind',lapply(1:length(outData), function(x) outData[[x]][[1]])),
-                    do.call('rbind',lapply(1:length(outData), function(x) outData[[x]][[2]])))
-    pb$tick()
-    return(outData)
-  })
 
-  estimates <- list(do.call('rbind',lapply(1:length(outData), function(x) outData[[x]][[1]])),
-                    do.call('rbind',lapply(1:length(outData), function(x) outData[[x]][[2]])))
+  estimates <- list(do.call('rbind',lapply(1:length(emResultsList), function(x) emResultsList[[x]][[1]])),
+                    do.call('rbind',lapply(1:length(emResultsList), function(x) emResultsList[[x]][[2]])))
 
   return(estimates)
 }
@@ -41,7 +31,7 @@ abundance_quantification <- function(read_classDT,mc.cores = 1,
 run_parallel <- function(g,conv.control,bias_correction,maxiter, read_classDT){
   tmp <- read_classDT[gene_sid==g]
   if((nrow(tmp)==1)){
-    outData <- list(data.table(tx_sid = tmp$tx_sid,
+    out <- list(data.table(tx_sid = tmp$tx_sid,
                                estimates = tmp$nobs,
                                gene_sid = tmp$gene_sid,
                                ntotal = sum(tmp$nobs)),
@@ -77,7 +67,7 @@ run_parallel <- function(g,conv.control,bias_correction,maxiter, read_classDT){
       b_est <- rep(0,ncol(a_mat))
     }
     nobs <- sum(n.obs)
-    outData <- list(data.table(tx_sid = rownames(a_mat),
+    out <- list(data.table(tx_sid = rownames(a_mat),
                                estimates = t_est,
                                gene_sid = g,
                                ntotal = nobs),
@@ -87,5 +77,5 @@ run_parallel <- function(g,conv.control,bias_correction,maxiter, read_classDT){
                                gene_sid = g,
                                ntotal = nobs))
   }
-  return(outData)
+  return(out)
 }
