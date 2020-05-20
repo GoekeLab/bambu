c15e5eef8d9c7d5e6576effea24e38c580c22e6a
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-05-20 19:35:20 +0800

    Update test_SEtoGTF.R

--- tests/testthat/test_SEtoGTF.R
+++ tests/testthat/test_SEtoGTF.R
@@ -1,4 +1,4 @@
-context("Generate GTF from bambu output")
+context("Generate GTF from summarizedExperiment object")
 
 test_that("SEtoGTF can generate a GTF file",{
   se <- readRDS(system.file("extdata", "seOutput_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu"))
