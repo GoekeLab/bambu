98c030de594e463232f9fad5ccd1fd20c7427c27
cying111 <chen_ying@gis.a-star.edu.sg>
2020-05-20 07:55:46 +0000

    Added plot for genes or isoforms of a gene with expression heatmap
    Former-commit-id: a4f52e101c5c8ea005391b0eb08b46becee22903

--- R/plotBambu.R
+++ R/plotBambu.R
@@ -2,7 +2,8 @@
 #' @title plot.bambu
 #' @param se An summarized experiment object obtained from \code\link{bambu} or \code{\link{transcriptToGene}}.
 #' @param group.variable Variable for grouping in plot, has be to provided if choosing to plot PCA.
-#' @param type plot type variable, a values of pca,  or heatmap, see \code{\link{details}}.
+#' @param type plot type variable, a values of annotation for a single gene with heatmap for isoform expressions,  pca,  or heatmap, see \code{\link{details}}.
+#' @param gene_id specifying the gene_id for plotting gene annotation, to be provided when type = "annotation".
 #' @details \code{\link{type}} indicates the type of plots to be plotted. There are two types of plots can be chosen, PCA or heatmap.
 #' @return A heatmap plot for all samples
 #' @importFrom stats prcomp
@@ -10,9 +11,48 @@
 #' @importFrom ComplexHeatmap Heatmap
 #' @importFrom circlize colorRamp2
 #' @importFrom RColorBrewer brewer.pal
+#' @importFrom ggbio autoplot
+#' @importFrom gridExtra grid.arrange
 #' @export
-plot.bambu <- function(se, group.variable = NULL, type ){
+plot.bambu <- function(se, group.variable = NULL, type , gene_id){
 
+  if(type == "annotation"){
+    if(is.null(gene_id)){
+      stop("Please provide the gene_id(s) of the gene of interest!")
+    }
+    if(ncol(rowData(se))==0){
+      if(!all(gene_id %in% rownames(se))){
+        stop("all(gene_id %in% rownames(se)) condition is not satisfied!")
+      }
+      geneRanges <- rowRanges(se)[gene_id]
+      names(geneRanges) <- paste0(gene_id,":", unlist(lapply(strand(geneRanges),function(x) unique(as.character(x)))))
+      
+      p_annotation <- ggbio::autoplot(geneRanges, group.selfish = TRUE)
+      p_expression <- ggbio::autoplot(as.matrix(log2(assays(se)$CPM[gene_id,]+1)))
+      
+      p <- gridExtra::grid.arrange(p_annotation@ggplot, p_expression)
+      return(p)
+    }else{
+      if(!all(gene_id %in% rowData(se)$GENEID)){
+        stop("all(gene_id %in% rowData(se)$GENEID) condition is not satisfied!")
+      }
+      p <- lapply(gene_id, function(g){
+        txVec <- rowData(se)[rowData(se)$GENEID == g,]$TXNAME
+        txRanges <- rowRanges(se)[txVec]
+        names(txRanges) <- paste0(txVec,":", unlist(lapply(strand(txRanges),function(x) unique(as.character(x)))))
+        p_annotation <- ggbio::autoplot(txRanges, group.selfish = TRUE)
+        p_expression <- ggbio::autoplot(as.matrix(log2(assays(se)$CPM[txVec,]+1)),axis.text.angle = 45)
+        
+        
+        p <- gridExtra::grid.arrange(p_annotation@ggplot, p_expression, top = g, heights = c(1,1))
+        return(p)
+      })
+      
+      return(p)
+    }
+  }
+  
+  #= 
   count.data <- assays(se)$CPM
   if(length(apply(count.data,1,sum)>10)>100){
     count.data <- count.data[apply(count.data,1,sum)>10,]
@@ -28,7 +68,7 @@ plot.bambu <- function(se, group.variable = NULL, type ){
         sample.info <- as.data.table(as.data.frame(colData(se)[,c("name",group.variable)]))
         setnames(sample.info, 1:2, c("runname","groupVar"))
 
-        pca_result <- prcomp(t(as.matrix(count.data)), scale = TRUE) ## can't really cluster them nicely
+        pca_result <- prcomp(t(as.matrix(count.data))) ## can't really cluster them nicely
         plotData <- data.table(pca_result$x[,1:2],keep.rownames = TRUE)
         setnames(plotData, 'rn','runname')
         if(!all(plotData$runname %in% sample.info$runname)){
@@ -43,7 +83,7 @@ plot.bambu <- function(se, group.variable = NULL, type ){
           theme_minimal()
 
       }else{
-        pca_result <- prcomp(t(as.matrix(count.data)), scale = TRUE) ## can't really cluster them nicely
+        pca_result <- prcomp(t(as.matrix(count.data))) ## can't really cluster them nicely
         plotData <- data.table(pca_result$x[,1:2],keep.rownames = TRUE)
         setnames(plotData, 'rn','runname')
 
@@ -61,20 +101,23 @@ plot.bambu <- function(se, group.variable = NULL, type ){
   if(type == "heatmap"){
 
     corData <- cor(count.data,method = "spearman")
-    col_fun = colorRamp2(seq(floor(range(corData)[1]*10)/10,ceiling(range(corData)[2]*10)/10,length.out = 8), brewer.pal(8,"Blues"))
+    col_fun = circlize::colorRamp2(seq(floor(range(corData)[1]*10)/10,ceiling(range(corData)[2]*10)/10,length.out = 8), RColorBrewer::brewer.pal(8,"Blues"))
 
     if(!is.null(group.variable)){
+      sample.info <- as.data.table(as.data.frame(colData(se)[,c("name",group.variable)]))
+      setnames(sample.info, 1:2, c("runname","groupVar"))
+      
       if(!all(colnames(count.data) %in% sample.info$runname)){
         stop("all(colnames(count.data) %in% sample.info$runname) is not satisfied!")
       }
-      topAnnotation <- HeatmapAnnotation(group = sample.info[match(colnames(count.data), runname)]$groupVar)
+      topAnnotation <- ComplexHeatmap::HeatmapAnnotation(group = sample.info[match(colnames(count.data), runname)]$groupVar)
 
-      p <- Heatmap(corData, name = "Sp.R", col = col_fun,
+      p <- ComplexHeatmap::Heatmap(corData, name = "Sp.R", col = col_fun,
                    top_annotation = topAnnotation)
 
     }else{
 
-      p <- Heatmap(corData, name = "Sp.R", col = col_fun)
+      p <- ComplexHeatmap::Heatmap(corData, name = "Sp.R", col = col_fun)
 
     }
     return(p)
