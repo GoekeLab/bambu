1594ab3d49050af2349533fc1964a25f658c7bc6
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-05-20 19:32:57 +0800

    create test_SEtoGTF.R draft

---
+++ tests/testthat/test_SEtoGTF.R
@@ -0,0 +1,8 @@
+context("Gene quantification")
+
+test_that("SEtoGTF can generate a GTF file",{
+  se <- readRDS(system.file("extdata", "seOutput_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu"))
+  gtf <- SEtoGTF(se)
+  expect_is(gtf, "data.frame")
+  expect_named(ncol(gtf), 9)
+  }
