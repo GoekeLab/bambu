a360ec391d1de8a42da168d5296db646b91828da
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-05-21 07:48:39 -0400

    Update test_SEtoGTF.R
    Former-commit-id: 6cb73797d39123ac92ce8a0e205dedd83143b31f

--- tests/testthat/test_SEtoGTF.R
+++ tests/testthat/test_SEtoGTF.R
@@ -4,5 +4,5 @@ test_that("SEtoGTF can generate a GTF file",{
   se <- readRDS(system.file("extdata", "seOutput_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu"))
   gtf <- SEtoGTF(se)
   expect_is(gtf, "data.frame")
-  expect_is(ncol(gtf), 9)
+  expect_equal(ncol(gtf), 9)
   })
