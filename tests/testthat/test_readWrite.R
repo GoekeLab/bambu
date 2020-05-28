a9011c0cf6d2e98419e1b88979e26d7fa3cf7491
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-05-27 21:06:13 -0400

    Update test_readWrite.R

--- tests/testthat/test_readWrite.R
+++ tests/testthat/test_readWrite.R
@@ -1,14 +1,5 @@
 context("Generate GTF file from summarizedExperiment object")
 
-test_that("write.bambu can generate a GTF by running write.gtf",{
-  se <- readRDS(system.file("extdata", "seOutput_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu"))
-  path <- "test_writebambu_gtf"
-  gtf <- write.bambu(se,path)
-  
-  expect_is(gtf, "data.frame")
-  expect_equal(ncol(gtf), 9)
-  })
-
 test_that("read.gtf can generate a GRangesList from a GTF file",{
   se <- readRDS(system.file("extdata", "seOutput_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu"))
   path <- "test_writebambu_gtf"
