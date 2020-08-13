be3e2fe041907e4e46da308ba0b026fa9679a5d3
cying111 <chen_ying@gis.a-star.edu.sg>
2020-08-13 07:39:11 +0000

    add function to modify read class with full length transcripts

--- R/annotationFunctions.R
+++ R/annotationFunctions.R
@@ -352,6 +352,59 @@ getEmptyClassFromSE <- function(se = se, annotationGrangesList = NULL){
 
 }
 
+#' Modify readClass to create full length read class
+#' @param readClassDt output from \code{getEmptyClassFromSE}
+#' @param annotationGrangesList inherits from \code{getEmptyClassFromSE}
+modifyReadClassWtFullLengthTranscript <- function(readClassDt, annotationGrangesList){
+  # get the minimum subset transcript for each read class
+  rcAnnotations <- data.table(as.data.frame(mcols(annotationGrangesList)))
+  setnames(rcAnnotations, c("eqClass","GENEID"),c("read_class_id","gene_id"))
+  
+  # match to read class
+  readClassDt_tmp <- rcAnnotations[readClassDt, on = c("read_class_id","gene_id")]
+  
+  # change concatenating symbol to & to avoid mis-spliting for novel transcript 
+  # find the possible first characters in the tx_id
+  uni_charVec <- unique(substr(readClassDt$tx_id,1,1))
+  for(uni_char in uni_charVec){
+    readClassDt_tmp[, read_class_id := gsub(paste0("\\.",uni_char,""),paste0("\\&",uni_char,""), read_class_id)]
+  }
+  
+  
+  # modify read class that can be found with a minimal subset transcript with a full length version 
+  # of the minimal subset transcript indicated by original transcript followed by Start
+  # this set by right, should be able to find all shared read class with a subset transcript matching and all transcript with unique 
+  # splicing 
+  readClassDt_tmp[!is.na(newTxClass), read_class_id_new:= paste0(read_class_id, "&", paste0(TXNAME,"Start")), by = read_class_id]
+  
+  # transcripts that are subset of shared read class, if they have a read class for themseleves, 
+  # modify by adding a full length version as well
+  readClassDt_tmp[!grepl(paste(paste0("(\\&",uni_charVec,")"), collapse = "|"), read_class_id)&(is.na(newTxClass)), read_class_id_new:=paste0(read_class_id, "&", paste0(tx_id,"Start"))]
+  
+  # for shared read class that do not have a subset transcript that is fully compatible with the splicing patterns, 
+  # do not modify 
+  readClassDt_tmp[is.na(read_class_id_new), read_class_id_new:=read_class_id]
+  
+  readClassDt_tmp[, `:=`(TXNAME = NULL, newTxClass = NULL, read_class_id = NULL, tx_id = NULL)]
+  readClassDt_tmp <- unique(readClassDt_tmp)
+  setnames(readClassDt_tmp, "read_class_id_new", "read_class_id")
+  
+  # split the new modify read class to obtain all matching transcripts
+  readClassDt <- unique(readClassDt_tmp[, list(tx_id = unlist(strsplit(read_class_id, "\\&"))), by = list(gene_id, nobs, read_class_id)],by=NULL)
+  
+  # for transcript that do not have an read class equal to the transcript, create the empty read class for that
+  
+  
+  # change concatenating symbol back to .
+  
+  for(uni_char in uni_charVec){
+    readClassDt[, read_class_id := gsub(paste0("\\&",uni_char,""),paste0("\\.",uni_char,""), read_class_id)]
+  }
+  
+   return(readClassDt)
+}
+
+
 #' From tx ranges to gene ranges
 #' @noRd
 txRangesToGeneRanges <- function(exByTx, TXNAMEGENEID_Map){
