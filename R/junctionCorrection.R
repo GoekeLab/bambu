d40320d57ec455d64994b79fcaa45b633134cfbf
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-09-09 04:12:36 -0400

    break down function to make function < 50 lines

--- R/junctionCorrection.R
+++ R/junctionCorrection.R
@@ -176,10 +176,12 @@ evalAnnotationOverlap <- function(intronRanges, uniqueAnnotatedIntrons, ignore.s
 {
   return(table(!is.na(GenomicRanges::match(intronRanges, uniqueAnnotatedIntrons,ignore.strand=ignore.strand))))
 }
+
+
 #############################################################################################
-# update junctions object if strand prediction improves overlap with annotations(Sep 8, 2020)
+# test start/end splice site given close by splice site (Sep 8, 2020)
 #############################################################################################
-testSpliceSites <- function(annotatedJunctionsStart, annotatedJunctions, junctionModel){
+testStartSpliceSites <- function(annotatedJunctionsStart, annotatedJunctions, junctionModel){
   mySet.all=((annotatedJunctionsStart$distStart.start!=0)&annotatedJunctionsStart$spliceStrand!='*'&annotatedJunctionsStart$startScore>0&(annotatedJunctionsStart$distStart.start<15))
   annotatedJunctions$spliceSitePredictionStart.start <- rep(NA, length(annotatedJunctions))
   if(any(mySet.all)){
@@ -200,66 +202,6 @@ testSpliceSites <- function(annotatedJunctionsStart, annotatedJunctions, junctio
     spliceSitePredictionStart.start[mySet.all]=predictions
     annotatedJunctions$spliceSitePredictionStart.start <- spliceSitePredictionStart.start[annotatedJunctions$junctionStartName]
   }
-  outputList <- list("annotatedJunctions" = annotatedJunctions, "junctionModel" = junctionModel)
-  return (outputList)
-}
-
-#' Predict splicing junctions
-#' @noRd
-predictSpliceJunctions <- function(annotatedJunctions, junctionModel=NULL, verbose = FALSE){
-  ##note: readibility can be improved using dplyr
-  annotatedJunctionsStart <- unique(GRanges(seqnames=seqnames(annotatedJunctions), ranges=IRanges(start=start(annotatedJunctions), end=start(annotatedJunctions)), strand='*', mcols(annotatedJunctions)[,c('startScore','junctionStartName','annotatedStart','spliceStrand','spliceMotif')]))
-  annotatedJunctionsStart$distStart.start <- c(0,(start(annotatedJunctionsStart[-1])-start(annotatedJunctionsStart[-length(annotatedJunctionsStart)]))*as.integer((seqnames(annotatedJunctionsStart[-1])==seqnames(annotatedJunctionsStart[-length(annotatedJunctionsStart)]))))
-  annotatedJunctionsStart$distStart.end=c((end(annotatedJunctionsStart[-length(annotatedJunctionsStart)])-end(annotatedJunctionsStart[-1]))*as.integer((seqnames(annotatedJunctionsStart[-length(annotatedJunctionsStart)])==seqnames(annotatedJunctionsStart[-1]))),0)
-  annotatedJunctionsStart$annotatedStart.start = c(FALSE,annotatedJunctionsStart$annotatedStart[-length(annotatedJunctionsStart)])
-  annotatedJunctionsStart$annotatedStart.end = c(annotatedJunctionsStart$annotatedStart[-1],FALSE)
-  annotatedJunctionsStart$startScore.start = c(FALSE,annotatedJunctionsStart$startScore[-length(annotatedJunctionsStart)])
-  annotatedJunctionsStart$startScore.end = c(annotatedJunctionsStart$startScore[-1],FALSE)
-  annotatedJunctionsStart$spliceStrand.start = c(FALSE,annotatedJunctionsStart$spliceStrand[-length(annotatedJunctionsStart)])
-  annotatedJunctionsStart$spliceStrand.end = c(annotatedJunctionsStart$spliceStrand[-1],FALSE)
-  annotatedJunctionsStart$spliceMotif.start = c(FALSE,annotatedJunctionsStart$spliceMotif[-length(annotatedJunctionsStart)])
-  annotatedJunctionsStart$spliceMotif.end = c(annotatedJunctionsStart$spliceMotif[-1],FALSE)
-
-  annotatedJunctionsEnd=sort(unique(GRanges(seqnames=seqnames(annotatedJunctions),ranges=IRanges(start=end(annotatedJunctions),end=end(annotatedJunctions)), strand='*', mcols(annotatedJunctions)[,c('endScore','junctionEndName','annotatedEnd','spliceStrand','spliceMotif')])))
-  annotatedJunctionsEnd$distEnd.start=c(0,(start(annotatedJunctionsEnd[-1])-start(annotatedJunctionsEnd[-length(annotatedJunctionsEnd)]))*as.integer((seqnames(annotatedJunctionsEnd[-1])==seqnames(annotatedJunctionsEnd[-length(annotatedJunctionsEnd)]))))
-  annotatedJunctionsEnd$distEnd.end=c((end(annotatedJunctionsEnd[-length(annotatedJunctionsEnd)])-end(annotatedJunctionsEnd[-1]))*as.integer((seqnames(annotatedJunctionsEnd[-length(annotatedJunctionsEnd)])==seqnames(annotatedJunctionsEnd[-1]))),0)
-  annotatedJunctionsEnd$annotatedEnd.start = c(FALSE,annotatedJunctionsEnd$annotatedEnd[-length(annotatedJunctionsEnd)])
-  annotatedJunctionsEnd$annotatedEnd.end = c(annotatedJunctionsEnd$annotatedEnd[-1],FALSE)
-  annotatedJunctionsEnd$endScore.start = c(FALSE,annotatedJunctionsEnd$endScore[-length(annotatedJunctionsEnd)])
-  annotatedJunctionsEnd$endScore.end = c(annotatedJunctionsEnd$endScore[-1],FALSE)
-  annotatedJunctionsEnd$spliceStrand.start = c(FALSE,annotatedJunctionsEnd$spliceStrand[-length(annotatedJunctionsEnd)])
-  annotatedJunctionsEnd$spliceStrand.end = c(annotatedJunctionsEnd$spliceStrand[-1],FALSE)
-  annotatedJunctionsEnd$spliceMotif.start = c(FALSE,annotatedJunctionsEnd$spliceMotif[-length(annotatedJunctionsEnd)])
-  annotatedJunctionsEnd$spliceMotif.end = c(annotatedJunctionsEnd$spliceMotif[-1],FALSE)
-  ## test start splice site given close by splice site (left/5')
-  if(is.null(junctionModel)){
-    junctionModelList <- list()
-  }
-  testStartSplice <- testSpliceSites(annotatedJunctionsStart, annotatedJunctions, junctionModel)
-  annotatedJunctions <- testStartSplice$annotatedJunctions
-  junctionModel <- testStartSplice$junctionModel
-  # mySet.all=((annotatedJunctionsStart$distStart.start!=0)&annotatedJunctionsStart$spliceStrand!='*'&annotatedJunctionsStart$startScore>0&(annotatedJunctionsStart$distStart.start<15))
-  # annotatedJunctions$spliceSitePredictionStart.start <- rep(NA, length(annotatedJunctions))
-  # if(any(mySet.all)){
-  #   mySet.training=(annotatedJunctionsStart$annotatedStart.start|annotatedJunctionsStart$annotatedStart)[mySet.all]
-  #   myData=data.frame(annotatedJunctionsStart$startScore/(annotatedJunctionsStart$startScore.start+annotatedJunctionsStart$startScore),annotatedJunctionsStart$startScore,annotatedJunctionsStart$distStart.start,(annotatedJunctionsStart$spliceStrand.start=='+'),annotatedJunctionsStart$spliceStrand.start=='-',(annotatedJunctionsStart$spliceStrand=='+'))[mySet.all,]
-  #   colnames(myData) <- paste('A',seq_len(ncol(myData)),sep='.')
-  #   modelmatrix=model.matrix(~A.1+A.2+A.3+A.4+A.5, data=data.frame((myData)))
-  #   if(is.null(junctionModel)){
-  #     myResults= fitBinomialModel(labels.train=as.integer(annotatedJunctionsStart$annotatedStart)[mySet.all][mySet.training], data.train=modelmatrix[mySet.training,], data.test=modelmatrix, show.cv=verbose, maxSize.cv=10000)
-  #     junctionModelList[['spliceSitePredictionStart.start']] <- myResults[[2]]
-  #     predictions = myResults[[1]]
-  #   }
-  #   else{
-  #     predictions= glmnet:::predict.cv.glmnet(junctionModel[['spliceSitePredictionStart.start']],newx=modelmatrix,s='lambda.min')
-  #   }
-  #   spliceSitePredictionStart.start <- rep(NA, length(annotatedJunctionsStart))
-  #   names(spliceSitePredictionStart.start) <- annotatedJunctionsStart$junctionStartName
-  #   spliceSitePredictionStart.start[mySet.all]=predictions
-  #   annotatedJunctions$spliceSitePredictionStart.start <- spliceSitePredictionStart.start[annotatedJunctions$junctionStartName]
-  # }
-  
-  ## test start splice site given close by splice site (right/3')
   mySet.all=((annotatedJunctionsStart$distStart.end!=0)&annotatedJunctionsStart$spliceStrand!='*'&annotatedJunctionsStart$startScore>0&abs(annotatedJunctionsStart$distStart.end)<15)
   annotatedJunctions$spliceSitePredictionStart.end <- rep(NA, length(annotatedJunctions))
   if(any(mySet.all)){
@@ -280,8 +222,10 @@ predictSpliceJunctions <- function(annotatedJunctions, junctionModel=NULL, verbo
     spliceSitePredictionStart.end[mySet.all]=predictions
     annotatedJunctions$spliceSitePredictionStart.end <- spliceSitePredictionStart.end[annotatedJunctions$junctionStartName]
   }
-  
-  ## test end splice site given close by splice site (start/5')
+  outputList <- list("annotatedJunctions" = annotatedJunctions, "junctionModel" = junctionModel)
+  return (outputList)
+}
+testEndSpliceSites <- function(annotatedJunctionsEnd, annotatedJunctions, junctionModel){
   mySet.all=(annotatedJunctionsEnd$distEnd.start!=0&annotatedJunctionsEnd$spliceStrand!='*'&annotatedJunctionsEnd$endScore>0&(annotatedJunctionsEnd$distEnd.start<15))
   annotatedJunctions$spliceSitePredictionEnd.start <- rep(NA, length(annotatedJunctions))
   if(any(mySet.all)){
@@ -302,8 +246,6 @@ predictSpliceJunctions <- function(annotatedJunctions, junctionModel=NULL, verbo
     spliceSitePredictionEnd.start[mySet.all]=predictions
     annotatedJunctions$spliceSitePredictionEnd.start <- spliceSitePredictionEnd.start[annotatedJunctions$junctionEndName]
   }
-  
-  ## test end splice site given close by splice site (right/3')
   mySet.all=(annotatedJunctionsEnd$distEnd.end!=0&annotatedJunctionsEnd$spliceStrand!='*'&annotatedJunctionsEnd$endScore>0&abs(annotatedJunctionsEnd$distEnd.end)<15)
   annotatedJunctions$spliceSitePredictionEnd.end <- rep(NA, length(annotatedJunctions))
   if(any(mySet.all)){
@@ -324,8 +266,47 @@ predictSpliceJunctions <- function(annotatedJunctions, junctionModel=NULL, verbo
     spliceSitePredictionEnd.end[mySet.all]=predictions
     annotatedJunctions$spliceSitePredictionEnd.end <- spliceSitePredictionEnd.end[annotatedJunctions$junctionEndName]
   }
-  if(is.null(junctionModel))
-  { junctionModel=junctionModelList}
+  outputList <- list("annotatedJunctions" = annotatedJunctions, "junctionModel" = junctionModel)
+  return (outputList)
+}
+#' Predict splicing junctions
+#' @noRd
+predictSpliceJunctions <- function(annotatedJunctions, junctionModel=NULL, verbose = FALSE){
+  ##note: readibility can be improved using dplyr
+  annotatedJunctionsStart <- unique(GRanges(seqnames=seqnames(annotatedJunctions), ranges=IRanges(start=start(annotatedJunctions), end=start(annotatedJunctions)), strand='*', mcols(annotatedJunctions)[,c('startScore','junctionStartName','annotatedStart','spliceStrand','spliceMotif')]))
+  annotatedJunctionsEnd=sort(unique(GRanges(seqnames=seqnames(annotatedJunctions),ranges=IRanges(start=end(annotatedJunctions),end=end(annotatedJunctions)), strand='*', mcols(annotatedJunctions)[,c('endScore','junctionEndName','annotatedEnd','spliceStrand','spliceMotif')])))
+  if(is.null(junctionModel)){junctionModelList <- list()}
+  ## test start splice site given close by splice site (left/5')
+  annotatedJunctionsStart$distStart.start <- c(0,(start(annotatedJunctionsStart[-1])-start(annotatedJunctionsStart[-length(annotatedJunctionsStart)]))*as.integer((seqnames(annotatedJunctionsStart[-1])==seqnames(annotatedJunctionsStart[-length(annotatedJunctionsStart)]))))
+  annotatedJunctionsStart$annotatedStart.start = c(FALSE,annotatedJunctionsStart$annotatedStart[-length(annotatedJunctionsStart)])
+  annotatedJunctionsStart$startScore.start = c(FALSE,annotatedJunctionsStart$startScore[-length(annotatedJunctionsStart)])
+  annotatedJunctionsStart$spliceStrand.start = c(FALSE,annotatedJunctionsStart$spliceStrand[-length(annotatedJunctionsStart)])
+  annotatedJunctionsStart$spliceMotif.start = c(FALSE,annotatedJunctionsStart$spliceMotif[-length(annotatedJunctionsStart)])
+  ## test start splice site given close by splice site (right/3')
+  annotatedJunctionsStart$distStart.end=c((end(annotatedJunctionsStart[-length(annotatedJunctionsStart)])-end(annotatedJunctionsStart[-1]))*as.integer((seqnames(annotatedJunctionsStart[-length(annotatedJunctionsStart)])==seqnames(annotatedJunctionsStart[-1]))),0)
+  annotatedJunctionsStart$annotatedStart.end = c(annotatedJunctionsStart$annotatedStart[-1],FALSE)
+  annotatedJunctionsStart$startScore.end = c(annotatedJunctionsStart$startScore[-1],FALSE)
+  annotatedJunctionsStart$spliceStrand.end = c(annotatedJunctionsStart$spliceStrand[-1],FALSE)
+  annotatedJunctionsStart$spliceMotif.end = c(annotatedJunctionsStart$spliceMotif[-1],FALSE)
+  testStartSplice <- testStartSpliceSites(annotatedJunctionsStart, annotatedJunctions, junctionModel)
+  annotatedJunctions <- testStartSplice$annotatedJunctions
+  junctionModel <- testStartSplice$junctionModel
+  ## test end splice site given close by splice site (start/5')
+  annotatedJunctionsEnd$distEnd.start=c(0,(start(annotatedJunctionsEnd[-1])-start(annotatedJunctionsEnd[-length(annotatedJunctionsEnd)]))*as.integer((seqnames(annotatedJunctionsEnd[-1])==seqnames(annotatedJunctionsEnd[-length(annotatedJunctionsEnd)]))))
+  annotatedJunctionsEnd$annotatedEnd.start = c(FALSE,annotatedJunctionsEnd$annotatedEnd[-length(annotatedJunctionsEnd)])
+  annotatedJunctionsEnd$endScore.start = c(FALSE,annotatedJunctionsEnd$endScore[-length(annotatedJunctionsEnd)])
+  annotatedJunctionsEnd$spliceStrand.start = c(FALSE,annotatedJunctionsEnd$spliceStrand[-length(annotatedJunctionsEnd)])
+  annotatedJunctionsEnd$spliceMotif.start = c(FALSE,annotatedJunctionsEnd$spliceMotif[-length(annotatedJunctionsEnd)])
+  ## test end splice site given close by splice site (right/3')
+  annotatedJunctionsEnd$distEnd.end=c((end(annotatedJunctionsEnd[-length(annotatedJunctionsEnd)])-end(annotatedJunctionsEnd[-1]))*as.integer((seqnames(annotatedJunctionsEnd[-length(annotatedJunctionsEnd)])==seqnames(annotatedJunctionsEnd[-1]))),0)
+  annotatedJunctionsEnd$annotatedEnd.end = c(annotatedJunctionsEnd$annotatedEnd[-1],FALSE)
+  annotatedJunctionsEnd$endScore.end = c(annotatedJunctionsEnd$endScore[-1],FALSE)
+  annotatedJunctionsEnd$spliceStrand.end = c(annotatedJunctionsEnd$spliceStrand[-1],FALSE)
+  annotatedJunctionsEnd$spliceMotif.end = c(annotatedJunctionsEnd$spliceMotif[-1],FALSE)
+  testEndSplice <- testEndSpliceSites(annotatedJunctionsEnd, annotatedJunctions, junctionModel)
+  annotatedJunctions <- testEndSplice$annotatedJunctions
+  junctionModel <- testEndSplice$junctionModel
+  if(is.null(junctionModel)){junctionModel = junctionModelList}
   return(list(annotatedJunctions, junctionModel))
 }
 
