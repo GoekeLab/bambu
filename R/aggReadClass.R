1431ab0c022b9cdbd1b7a817461a398f9d375b5d
cying111 <chen_ying@gis.a-star.edu.sg>
2020-09-10 22:13:52 +0800

    shorten lines

--- R/aggReadClass.R
+++ R/aggReadClass.R
@@ -2,7 +2,8 @@
 #' @param dt A data.table object
 #' @noRd
 aggReadClass <- function(dt){
-  dt[, eqClass:=paste(sort(unique(tx_sid)), collapse = '.'), by = list(read_class_sid,gene_sid)]
+  dt[, eqClass:=paste(sort(unique(tx_sid)), collapse = '.'), 
+     by = list(read_class_sid,gene_sid)]
   dt[, read_class_sid_stored:=read_class_sid]
   eqClassVec <- unique(dt$eqClass)
   dt[, read_class_sid:=match(eqClass,eqClassVec)]
@@ -11,6 +12,7 @@ aggReadClass <- function(dt){
   tmp <- unique(dt[,.(read_class_sid, read_class_sid_stored, nobs_stored)])
   tmp[, nobs:=sum(nobs_stored), by = read_class_sid]
   tmp <- unique(tmp[,.(read_class_sid, nobs)])
-  dt <- unique(dt[,.(read_class_sid, tx_sid,gene_sid)])[tmp, on = 'read_class_sid']
+  dt <- unique(dt[,.(read_class_sid, 
+                     tx_sid,gene_sid)])[tmp, on = 'read_class_sid']
   return(list(dt,eqClassVec))
 }
