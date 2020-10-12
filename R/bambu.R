f70a918bdad3a99ecb15a7c61c32717340174ed7
cying111 <chen_ying@gis.a-star.edu.sg>
2020-10-12 17:39:56 +0800

    remove the small bam file as not much increase in checking speed

--- R/bambu.R
+++ R/bambu.R
@@ -58,7 +58,7 @@
 #' @examples
 #' ## =====================
 #' test.bam <- system.file("extdata",
-#'     "SGNex_A549_directRNA_replicate5_run1_small.bam",
+#'     "SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.bam",
 #'     package = "bambu")
 #' fa.file <- system.file("extdata", 
 #'     "Homo_sapiens.GRCh38.dna_sm.primary_assembly_chr9_1_1000000.fa", 
