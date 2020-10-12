5bd3ff70ea3d40b6494db81544b20f82df19817b
cying111 <chen_ying@gis.a-star.edu.sg>
2020-10-12 13:25:21 +0800

    update examples for plotBambu

--- R/plotBambu.R
+++ R/plotBambu.R
@@ -166,11 +166,7 @@ plotHeatmap <- function(se, count.data, group.variable) {
 #' se <- readRDS(system.file("extdata",
 #' "seOutputCombined_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds",
 #' package = "bambu"))
-#' colnames(se) <- colData(se)$name <- c("sample1", "sample2")
-#' assays(se)$CPM[, 2] <- pmax(0, rnorm(
-#'     length(assays(se)$CPM[, 2]),
-#'     assays(se)$CPM[, 2], 10))
-#' plotBambu(se, type = "heatmap")
+#' plotBambu(se, type = "PCA")
 plotBambu <- function(se, group.variable = NULL,
     type = c("annotation", "pca", "heatmap"),
     gene_id = NULL, transcript_id = NULL) {
