a3f46e3e20b7a907f58dd5c28d72aaed20a449b9
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-12-23 14:44:05 +0800

    Update and rename test_annotateSpliceOverlapByDist.R to test_compareTranscripts.R

--- tests/testthat/test_compareTranscripts.R
+++ tests/testthat/test_compareTranscripts.R
@@ -4,7 +4,7 @@ test_that("annotateSpliceOverlapByDist can generate a dataframe with annotation"
   query <- readRDS(system.file("extdata", "annotateSpliceOverlapByDist_testQuery.rds", package = "bambu"))
   subject <- readRDS(system.file("extdata", "annotateSpliceOverlapByDist_testSubject.rds", package = "bambu"))
   refTab <- readRDS(system.file("extdata", "annotateSpliceOverlapsByDist_refoutput.rds", package = "bambu"))
-  tab <- annotateSpliceOverlapsByDist(query, subject)
+  tab <- compareTranscripts(query, subject)
   expect_is(tab, class = 'data.frame')
   expect_equal(tab, refTab[, colnames(tab)])
 })
