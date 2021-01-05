d2005172ff80e0d21105026167fb83d93d307d4f
jonathangoeke <10560051+jonathangoeke@users.noreply.github.com>
2021-01-05 11:17:33 +0800

    moved bambu.constructReadClass to bambu-processReads.R

--- R/bambu-processReads.R
+++ R/bambu-processReads.R
@@ -48,3 +48,41 @@ bambu.processReads <- function(reads, readClass.file, annotations, genomeSequenc
   
   return(readClassList)
 }
+
+#' Preprocess bam files and save read class files
+#' @inheritParams bambu
+#' @noRd
+bambu.constructReadClass <- function(bam.file, genomeSequence, annotations,
+                                     readClass.outputDir = NULL, stranded = FALSE, ncore = 1, verbose = FALSE) {
+  readGrgList <- prepareDataFromBam(bam.file[[1]], ncore = ncore,
+                                    verbose = verbose)
+  if (length(intersect(GenomeInfoDb::seqlevels(readGrgList),
+                       GenomeInfoDb::seqlevels(annotations))) == 0)
+    stop("Error: please provide annotation with matched seqlevel styles.")
+  se <- isore.constructReadClasses(
+    readGrgList = readGrgList,
+    runName = names(bam.file)[1],
+    annotationGrangesList = annotations,
+    genomeSequence = genomeSequence,
+    stranded = stranded,
+    ncore = ncore,
+    verbose = verbose)
+  GenomeInfoDb::seqlevels(se) <- unique(c(GenomeInfoDb::seqlevels(se),
+                                          GenomeInfoDb::seqlevels(annotations)))
+  if (!is.null(readClass.outputDir)) {
+    readClassFile <- paste0(readClass.outputDir,names(bam.file),
+                            "_readClassSe.rds")
+    if (file.exists(readClassFile)) {
+      show(paste(readClassFile, "exists, will be overwritten"))
+      # warning is not printed, use show in addition
+      warning(paste(readClassFile, "exists, will be overwritten"))
+    } else {
+      readClassFile <- BiocFileCache::bfcnew(BiocFileCache::BiocFileCache(
+        readClass.outputDir, ask = FALSE),
+        paste0(names(bam.file),"_readClassSe"), ext = ".rds")
+    }
+    saveRDS(se, file = readClassFile)
+    se <- readClassFile
+  }
+  return(se)
+}
