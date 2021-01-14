cbbb43ac96debf31311fd28c82f18c62c3f16446
jonathangoeke <10560051+jonathangoeke@users.noreply.github.com>
2021-01-14 14:18:20 +0800

    resolve merge conflicts (spacing)

--- R/bambu-extendAnnotations.R
+++ R/bambu-extendAnnotations.R
@@ -13,24 +13,23 @@ bambu.extendAnnotations <- function(readClassList, annotations,
     }
     combinedTxCandidates <- isore.combineTranscriptCandidates(readClass,
         readClassSeRef = combinedTxCandidates, verbose = verbose)}
-  end.ptm <- proc.time()
-  if (verbose) message("combining transcripts in ",
-                       round((end.ptm - start.ptm)[3] / 60, 1)," mins.")
-  annotations <- isore.extendAnnotations(
-    se = combinedTxCandidates,
-    annotationGrangesList = annotations,
-    remove.subsetTx = isoreParameters[["remove.subsetTx"]],
-    min.readCount = isoreParameters[["min.readCount"]],
-    min.readFractionByGene = isoreParameters[["min.readFractionByGene"]],
-    min.sampleNumber = isoreParameters[["min.sampleNumber"]],
-    min.exonDistance = isoreParameters[["min.exonDistance"]],
-    min.exonOverlap = isoreParameters[["min.exonOverlap"]],
-    min.primarySecondaryDist = 
+    end.ptm <- proc.time()
+    if (verbose) message("combining transcripts in ",
+        round((end.ptm - start.ptm)[3] / 60, 1)," mins.")
+    annotations <- isore.extendAnnotations(
+      se = combinedTxCandidates,
+      annotationGrangesList = annotations,
+      remove.subsetTx = isoreParameters[["remove.subsetTx"]],
+      min.readCount = isoreParameters[["min.readCount"]],
+      min.readFractionByGene = isoreParameters[["min.readFractionByGene"]],
+      min.sampleNumber = isoreParameters[["min.sampleNumber"]],
+      min.exonDistance = isoreParameters[["min.exonDistance"]],
+      min.exonOverlap = isoreParameters[["min.exonOverlap"]],
+      min.primarySecondaryDist = 
       isoreParameters[['min.primarySecondaryDist']], 
-    min.primarySecondaryDistStartEnd = 
+      min.primarySecondaryDistStartEnd = 
       isoreParameters[['min.primarySecondaryDistStartEnd1']],
-    prefix = isoreParameters[["prefix"]],
-    verbose = verbose
-  )
-  return(annotations)
+      prefix = isoreParameters[["prefix"]],
+      verbose = verbose)
+    return(annotations)
 }
