9c4dcf2573d11b0c97c0bcb9157a1336734e6cf1
cying111 <chen_ying@gis.a-star.edu.sg>
2021-01-14 11:36:02 +0800

    update tests

--- tests/testthat/test_bambu.R
+++ tests/testthat/test_bambu.R
@@ -3,21 +3,25 @@ context("Isoform quantification")
 # bambu.quantDT
 test_that("generic function of isoform quantification of data.table is list of 2", {
     # test case
-    #   1: Overlapping scenario with no read support for each transcript
-    #   2: Same scenario as in 1 but no empty class
-    #   3: Transcripts not overlapping
-    #   4: Second overlapping scenario with read support for each transcript
-    #   5: A real example of observed counts with complex overlapping scenario
+    #   1: simplest case with no overlap
+    #   2: simple overlapping scenario (not subset)
+    #   3: subset scenario 1 with subset transcript being highly expressed 
+    #   4: subset scenario 2 with long transcript being highly expressed
+    #   5: highly similar transcripts scenario 
 
     lapply(1:5, function(s) {
-        est <- bambu.quantDT(readClassDt = get(paste0("data", s)))
+        est <- bambu.quantDT(readClassDt = get(paste0("data", s)),
+            emParameters = list(bias = FALSE, maxiter = 10000, conv = 10^(-2), 
+            minvalue = 10^(-8)))
         expect_type(est, "list")
         expect_equal(est, estOutput_woBC[[s]])
     })
 
     ## with bias correction
     lapply(1:5, function(s) {
-        est <- bambu.quantDT(readClassDt = get(paste0("data", s)))
+        est <- bambu.quantDT(readClassDt = get(paste0("data", s)),
+            emParameters = list(bias = TRUE, maxiter = 10000, conv = 10^(-2), 
+            minvalue = 10^(-8)))
         expect_type(est, "list")
         expect_equal(est, estOutput_wBC[[s]])
     })
@@ -92,12 +96,12 @@ test_that("bambu (isoform quantification of bam file and save readClassFiles) pr
 
 test_that("bambu (isoform quantification of saved readClassFiles) produces expected output", {
     ## ToDo: update data sets for comparison
-    seReadClass1 <- system.file("extdata", "seReadClass_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu")
+    seReadClass1 <- system.file("extdata", "seReadClassUnstranded_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu")
     gr <- readRDS(system.file("extdata", "annotationGranges_txdbGrch38_91_chr9_1_1000000.rds", package = "bambu"))
 
 
     seExtendedExpected <- readRDS(system.file("extdata", "seOutputExtended_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu"))
-    seCombinedExpected <- readRDS(system.file("extdata", "seOutputCombined_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu"))
+    seCombinedExpected <- readRDS(system.file("extdata", "seOutputCombined2_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu"))
 
 
     # test case 1: bambu with single bam file, only using annotations (default option)
@@ -110,9 +114,6 @@ test_that("bambu (isoform quantification of saved readClassFiles) produces expec
     # test case 2: bambu with multiple bam file, only using annotations (default option), yieldSize lower than read count
     set.seed(1234)
     seCombined <- bambu(rcFile = c(seReadClass1, seReadClass1), annotations = gr, discovery = FALSE)
-    dimnames(assays(seCombinedExpected)$counts)[[2]] <- dimnames(assays(seCombinedExpected)$CPM)[[2]] <- colnames(seCombinedExpected) <- colData(seCombinedExpected)$name <- gsub(".bam", "", colnames(seCombinedExpected))
-    dimnames(seCombined@assays@data@listData$counts)[[1]] <- dimnames(seCombined@assays@data@listData$CPM)[[1]] <- rownames(seCombinedExpected)
-
     expect_s4_class(seCombined, "SummarizedExperiment")
     expect_equal(seCombined, seCombinedExpected)
 })
