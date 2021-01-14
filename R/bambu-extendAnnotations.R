ee53c9e0c55409b24040daa7eba0d2b3b6e14f02
jonathangoeke <10560051+jonathangoeke@users.noreply.github.com>
2021-01-14 14:19:22 +0800

    solve merge conflict (spaces)

--- R/bambu-extendAnnotations.R
+++ R/bambu-extendAnnotations.R
@@ -17,19 +17,19 @@ bambu.extendAnnotations <- function(readClassList, annotations,
     if (verbose) message("combining transcripts in ",
         round((end.ptm - start.ptm)[3] / 60, 1)," mins.")
     annotations <- isore.extendAnnotations(
-      se = combinedTxCandidates,
-      annotationGrangesList = annotations,
-      remove.subsetTx = isoreParameters[["remove.subsetTx"]],
-      min.readCount = isoreParameters[["min.readCount"]],
-      min.readFractionByGene = isoreParameters[["min.readFractionByGene"]],
-      min.sampleNumber = isoreParameters[["min.sampleNumber"]],
-      min.exonDistance = isoreParameters[["min.exonDistance"]],
-      min.exonOverlap = isoreParameters[["min.exonOverlap"]],
-      min.primarySecondaryDist = 
-      isoreParameters[['min.primarySecondaryDist']], 
-      min.primarySecondaryDistStartEnd = 
-      isoreParameters[['min.primarySecondaryDistStartEnd1']],
-      prefix = isoreParameters[["prefix"]],
-      verbose = verbose)
+        se = combinedTxCandidates,
+        annotationGrangesList = annotations,
+        remove.subsetTx = isoreParameters[["remove.subsetTx"]],
+        min.readCount = isoreParameters[["min.readCount"]],
+        min.readFractionByGene = isoreParameters[["min.readFractionByGene"]],
+        min.sampleNumber = isoreParameters[["min.sampleNumber"]],
+        min.exonDistance = isoreParameters[["min.exonDistance"]],
+        min.exonOverlap = isoreParameters[["min.exonOverlap"]],
+        min.primarySecondaryDist = 
+        isoreParameters[['min.primarySecondaryDist']], 
+        min.primarySecondaryDistStartEnd = 
+        isoreParameters[['min.primarySecondaryDistStartEnd1']],
+        prefix = isoreParameters[["prefix"]],
+        verbose = verbose)
     return(annotations)
 }
