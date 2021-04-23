b5c7417e7c31cfde6c99d9abe42c5b16e6427312
Andre Sim <andre.sim@hotmail.com>
2021-04-23 18:01:03 +0800

    move assignGeneID test to same SE

--- tests/testthat/test_assignGeneID.R
+++ tests/testthat/test_assignGeneID.R
@@ -1,7 +1,7 @@
 context("bambu-processReads_utilityConstructReadClasses")
 
 test_that("assignGeneId returns correct gene ids",{
-  se <- readRDS(system.file("extdata", "proto_seOutput_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu"))
+  se <- readRDS(system.file("extdata", "seReadClassUnstranded_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.rds", package = "bambu"))
   gr <- readRDS(system.file("extdata", "annotationGranges_txdbGrch38_91_chr9_1_1000000.rds", package = "bambu"))
   rowData(se)$GENEIDs = NA
   expectedGENEIDs = unname(unlist(read.table(system.file("extdata", "test_GENEIDs_seOutput_SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000.csv", package = "bambu"))))
