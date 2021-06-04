adcf82622a537079a4019d8990cb01ee8e43349e
Chen Ying <chen_ying@gis.a-star.edu.sg>
2021-06-04 09:55:34 +0800

    add brackets for each filtering condition

--- R/bambu-extendAnnotations-utilityExtend.R
+++ R/bambu-extendAnnotations-utilityExtend.R
@@ -56,8 +56,8 @@ isore.extendAnnotations <- function(combinedTranscripts, annotationGrangesList,
 filterTranscriptsByRead <- function(combinedTranscripts, min.sampleNumber){
   if (nrow(combinedTranscripts) > 0) 
     filterSet <- combinedTranscripts$NSampleReadCount >= min.sampleNumber & (
-        combinedTranscripts$NSampleReadProp >= min.sampleNumber &
-        combinedTranscripts$NSampleGeneFDR >= min.sampleNumber &
+        combinedTranscripts$NSampleReadProp >= min.sampleNumber) & (
+        combinedTranscripts$NSampleGeneFDR >= min.sampleNumber) & (
         combinedTranscripts$NSampleTxFDR >= min.sampleNumber)
   # filter based on read count and transcript usage
   return(filterSet)
