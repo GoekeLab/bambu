1b240f24ab117e0f23c36e2e6c2b55de8e864c7c
Andre <andre_sim@gis.a-star.edu.sg>
2021-07-09 11:07:43 +0800

    Move checkInputSequence to bambu-utilityFunctions

--- R/bambu-processReads.R
+++ R/bambu-processReads.R
@@ -125,30 +125,3 @@ seqlevelCheckReadsAnnotation <- function(reads, annotations){
             annotations might be incomplete. Please compare objects
             on the same reference")
 }
-
-#' Function to create a object that can be queried by getSeq
-#' Either from fa file, or BSGenome object
-#' @importFrom methods is
-#' @importFrom Rsamtools FaFile
-#' @noRd
-checkInputSequence <- function(genomeSequence) {
-    if (is.null(genomeSequence)) stop("Reference genome sequence is missing,
-        please provide fasta file or BSgenome name, see available.genomes()")
-    if (is(genomeSequence, "character")) {
-        if (grepl(".fa", genomeSequence)) {
-            if (.Platform$OS.type == "windows") {
-                genomeSequence <- Biostrings::readDNAStringSet(genomeSequence)
-                newlevels <- unlist(lapply(strsplit(names(genomeSequence)," "),
-                    "[[", 1))
-                names(genomeSequence) <- newlevels
-            } else {
-                indexFileExists <- file.exists(paste0(genomeSequence,".fai"))
-                if (!indexFileExists) indexFa(genomeSequence)
-                genomeSequence <- FaFile(genomeSequence)
-            }
-        } else {
-            genomeSequence <- BSgenome::getBSgenome(genomeSequence)
-        }
-    }
-    return(genomeSequence)
-}
