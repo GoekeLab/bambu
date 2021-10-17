3a94e9952357cd5b1432f4ecf9424f292699f0db
Chen Ying <chen_ying@gis.a-star.edu.sg>
2021-10-18 01:21:52 +0800

    update test datasets to allow tests pass

--- tests/testthat/test_assignGeneID.R
+++ tests/testthat/test_assignGeneID.R
@@ -1,9 +1,12 @@
 context("bambu-processReads_utilityConstructReadClasses")
 
 test_that("assignGeneId returns correct gene ids",{
-  se <- readRDS("C:/Users/simandred/Documents/GitHub/bambu/tests/testData/test_se.rds")
-  annotations <- readRDS("C:/Users/simandred/Documents/GitHub/bambu/tests/testData/test_annotations.rds")
-  seExpected = readRDS("C:/Users/simandred/Documents/GitHub/bambu/tests/testData/SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000_readClassSe.rds")
+    se <- readRDS(system.file("extdata", "test_se.rds", package = "bambu"))
+    annotations <- readRDS(system.file("extdata", "test_annotations.rds", 
+        package = "bambu"))
+    seExpected = readRDS(system.file("extdata", 
+        "SGNex_A549_directRNA_replicate5_run1_chr9_1_1000000_readClassSe.rds", 
+        package = "bambu"))
   expect_equal(assignGeneIds(rowRanges(se), annotations), rowData(seExpected)$GENEID)
 })
 
