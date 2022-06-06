0966c2afc86f4dd20d3be456338a44c7645eeacf
minhao <lingminhao31@gmail.com>
2022-06-06 11:01:19 +0800

    update test function

--- tests/testthat/test_prepareAnnotations.R
+++ tests/testthat/test_prepareAnnotations.R
@@ -3,13 +3,13 @@ context("Prepare annotations")
 test_that("prepareAnnotations of txdb object is a GRangesList", {
     txdb <- AnnotationDbi::loadDb(system.file("extdata", "Homo_sapiens.GRCh38.91.annotations-txdb_chr9_1_1000000.sqlite", package = "bambu"))
 
-    expectedGR <- readRDS(system.file("extdata", "annotationGranges_txdbGrch38_91_chr9_1_1000000.rds", package = "bambu"))
+    expectedGR <- readRDS(system.file("extdata", "annotationGranges_txdb2Grch38_91_chr9_1_1000000.rds", package = "bambu"))
 
     gr <- prepareAnnotations(x = txdb)
 
     expect_equal(gr, expectedGR)
     expect_s4_class(gr, class = "CompressedGRangesList")
-    expect_named(mcols(gr), c("TXNAME", "GENEID", "eqClass"))
+    expect_named(mcols(gr), c("TXNAME", "GENEID", "txid", "eqClass", "eqClassById"))
 })
 
 
@@ -18,10 +18,8 @@ test_that("prepareAnnotations of genome library is a GRangesList", {
     gr <- prepareAnnotations(TxDb.Hsapiens.UCSC.hg38.knownGene)
 
     expect_s4_class(gr, class = "CompressedGRangesList")
-    expect_named(mcols(gr), c("TXNAME", "GENEID", "eqClass"))
+    expect_named(mcols(gr), c("TXNAME", "GENEID", "txid", "eqClass", "eqClassById"))
 })
-
-
 
 
 test_that("prepareAnnotations from gtf file is GRangesList", {
@@ -33,5 +31,5 @@ test_that("prepareAnnotations from gtf file is GRangesList", {
 
     expect_equal(gr[order(names(gr))], expectedGR[order(names(expectedGR))])
     expect_s4_class(gr, class = "CompressedGRangesList")
-    expect_named(mcols(gr), c("TXNAME", "GENEID", "eqClass"))
+    expect_named(mcols(gr), c("TXNAME", "GENEID", "txid", "eqClass", "eqClassById"))
 })
