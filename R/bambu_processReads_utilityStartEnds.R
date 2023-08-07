2bfc3103315cd8495e96f03a6c7dce77d6e033cc
Andre <andre_sim@gis.a-star.edu.sg>
2023-08-07 14:34:33 +0800

    Add polyA tail lengths to rowData

---
+++ R/bambu_processReads_utilityStartEnds.R
@@ -0,0 +1,25 @@
+findPolyATail = function(reads, softClipPrime, polyAPattern, polyAPatternLong, mat){
+    polyASeqPrime <- GRanges(seqnames = c(1:length(reads)),
+        IRanges(rep(1,length(reads)), width=softClipPrime))
+    x = mcols(reads)$seq
+    names(x) = c(1:length(reads))
+    polyASeqPrime = BSgenome::getSeq(x, polyASeqPrime)
+    paPrimeTable <- barcodeAlignmentExtended(
+        pattern=polyAPattern, 
+        subject=polyASeqPrime,
+        type='overlap',
+        gapOpening=-2,
+        gapExtension= -3,
+        substitutionMatrix=mat)
+    extendedSet <- which(paPrimeTable[,'score']> (0.95*searchLengthUnaligned))
+    paPrimeTableLong <- barcodeAlignmentExtended(
+        pattern=polyAPatternLong, 
+        subject=polyASeqPrime[extendedSet], 
+        type='overlap',
+        gapOpening=-2,
+        gapExtension= -3,
+        substitutionMatrix=mat)
+    paPrimeTable[extendedSet,] <- paPrimeTableLong
+    return(paPrimeTable[,'score'])
+
+}
--- R/prepareDataFromBam.R
+++ R/prepareDataFromBam.R
@@ -25,20 +25,33 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE, use.n
     bf <- open(bamFile)
     readGrgList <- list()
     counter <- 1
+    #polyA variables
+    searchLengthUnaligned = 20
+    mat <- nucleotideSubstitutionMatrix(match = 1, mismatch = -2, baseOnly = TRUE)
+    polyAPattern=DNAStringSet(paste0(rep('T',searchLengthUnaligned),collapse=''))  
+    polyAPatternLong=DNAStringSet(paste0(rep('T',2000),collapse=''))  
+    polyTPattern=DNAStringSet(paste0(rep('A',searchLengthUnaligned),collapse=''))  
+    polyTPatternLong=DNAStringSet(paste0(rep('A',2000),collapse=''))  
+    ###
     while (isIncomplete(bf)) {
         reads = readGAlignments(bf,
             param = ScanBamParam(flag =
-                scanBamFlag(isSecondaryAlignment = FALSE), what = "cigar"),
+                scanBamFlag(isSecondaryAlignment = FALSE), what = "seq"),
             use.names = use.names)
         readGrgList[[counter]] = grglist(reads)
-        softClip5Prime <-pmax(0,as.numeric(gsub('^(\\d*)[S].*','\\1',mcols(reads)$cigar)), na.rm=T)
-        softClip3Prime <-pmax(0,as.numeric(gsub('.*\\D(\\d*)[S]$','\\1',mcols(reads)$cigar)), na.rm=T)
-        hardClip5Prime <-pmax(0,as.numeric(gsub('^(\\d*)[H].*','\\1',mcols(reads)$cigar)), na.rm=T)
-        hardClip3Prime <-pmax(0,as.numeric(gsub('.*\\D(\\d*)[H]$','\\1',mcols(reads)$cigar)), na.rm=T)
+        softClip5Prime <-pmax(0,as.numeric(gsub('^(\\d*)[S].*','\\1',GenomicAlignments::cigar(reads))), na.rm=T)
+        softClip3Prime <-pmax(0,as.numeric(gsub('.*\\D(\\d*)[S]$','\\1',GenomicAlignments::cigar(reads))), na.rm=T)
+        hardClip5Prime <-pmax(0,as.numeric(gsub('^(\\d*)[H].*','\\1',GenomicAlignments::cigar(reads))), na.rm=T)
+        hardClip3Prime <-pmax(0,as.numeric(gsub('.*\\D(\\d*)[H]$','\\1',GenomicAlignments::cigar(reads))), na.rm=T)
         mcols(readGrgList[[counter]])$softClip5Prime = softClip5Prime
         mcols(readGrgList[[counter]])$softClip3Prime = softClip3Prime
         mcols(readGrgList[[counter]])$hardClip5Prime = hardClip5Prime
         mcols(readGrgList[[counter]])$hardClip3Prime = hardClip3Prime
+
+        #todo: seperate this by alignment strand
+        mcols(readGrgList[[counter]])$polyA5 = findPolyATail(reads, softClip5Prime, polyAPattern, polyAPatternLong, mat)
+        mcols(readGrgList[[counter]])$polyA3 = findPolyATail(reads, softClip3Prime, polyTPattern, polyTPatternLong, mat)
+        
         counter <- counter + 1
     }
     on.exit(close(bf))
@@ -51,5 +64,18 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE, use.n
     readGrgList <- readGrgList[width(readGrgList) > 1]
     mcols(readGrgList)$id <- seq_along(readGrgList)
     return(readGrgList)
+}
+
+barcodeAlignmentExtended <-  function(pattern, subject,type='local-global',...)  ## should replace function above, contains percent identity (pid)
+{
+  data <- matrix(NA,ncol=4,nrow=length(subject))
+  colnames(data) <- c('score','pid','start', 'end')
+  #  seq.align <- pairwiseAlignment(pattern=rep(DNAStringSet(pattern),length(subject)),subject=subject,type='global-local',...)
+  seq.align <- pairwiseAlignment(pattern=subject,subject=pattern,type=type,...)
+  data[,'score'] <- score(seq.align)
+  data[,'pid'] <- pid(seq.align)
+  data[,'start'] <- start(pattern(seq.align))
+  data[,'end'] <- end(pattern(seq.align))
+  return(data)
 }
 
