c392d086f9d42155ae2f384f6e367e9a63013af9
Andre <andre_sim@gis.a-star.edu.sg>
2024-02-08 11:12:04 +0800

    Keep readnames even if demultiplexed

--- R/prepareDataFromBam.R
+++ R/prepareDataFromBam.R
@@ -27,6 +27,7 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE, use.n
     counter <- 1
     cells <- c()
     umi <- c()
+    use.names.OG = use.names
     if(demultiplexed) use.names = TRUE
     while (isIncomplete(bf)) {
         ### add ###
@@ -43,7 +44,7 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE, use.n
             mcols(readGrgList[[counter]])$UMI <- ifelse(!is.na(mcols(alignmentInfo)$UG), mcols(alignmentInfo)$UG, 
                                                        substr(names(readGrgList[[counter]]), 18, 29))
             
-            names(readGrgList[[counter]]) <- NULL 
+            if(!use.names.OG) {names(readGrgList[[counter]]) <- NULL }
             cells <- unique(c(cells, mcols(readGrgList[[counter]])$CB))
             mcols(readGrgList[[counter]])$CB <- factor(mcols(readGrgList[[counter]])$CB, levels = cells)
             umi <- unique(c(umi, mcols(readGrgList[[counter]])$UMI))
