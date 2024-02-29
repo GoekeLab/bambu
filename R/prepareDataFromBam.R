6ec31de63a714844c322b16fe33d73740e520362
Andre <andre_sim@gis.a-star.edu.sg>
2024-02-29 13:57:28 +0800

    Tidy up clean reads

--- R/prepareDataFromBam.R
+++ R/prepareDataFromBam.R
@@ -7,7 +7,8 @@
 #' @importFrom GenomicAlignments grglist readGAlignments
 #' @importFrom GenomicRanges width
 #' @noRd
-prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE, use.names = FALSE, demultiplexed = NULL, cleanReads = FALSE) {
+prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE, 
+        use.names = FALSE, demultiplexed = FALSE, cleanReads = FALSE) {
     if (is(bamFile, "BamFile")) {
         if (!is.null(yieldSize)) {
             yieldSize(bamFile) <- yieldSize
@@ -28,7 +29,7 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE, use.n
     cells <- c()
     umi <- c()
     use.names.OG = use.names
-    if(demultiplexed) use.names = TRUE
+    if(demultiplexed | cleanReads) use.names = TRUE
     while (isIncomplete(bf)) {
         alignmentInfo <- readGAlignments(bf, param = ScanBamParam(tag = c("BC", "UG"), 
                                          flag = scanBamFlag(isSecondaryAlignment = FALSE)), 
@@ -41,21 +42,20 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE, use.n
             mcols(readGrgList[[counter]])$UMI <- ifelse(!is.na(mcols(alignmentInfo)$UG), mcols(alignmentInfo)$UG, 
                                                        substr(names(readGrgList[[counter]]), 18, 29))
             
-            if(!use.names.OG) {names(readGrgList[[counter]]) <- NULL }
             cells <- unique(c(cells, mcols(readGrgList[[counter]])$CB))
             mcols(readGrgList[[counter]])$CB <- factor(mcols(readGrgList[[counter]])$CB, levels = cells)
             umi <- unique(c(umi, mcols(readGrgList[[counter]])$UMI))
             mcols(readGrgList[[counter]])$UMI <- factor(mcols(readGrgList[[counter]])$UMI, levels = umi)
         }
         if(cleanReads){
-            softClip5Prime <-pmax(0,as.numeric(gsub('^(\\d*)[S].*','\\1',GenomicAlignments::cigar(reads))), na.rm=T)
-            softClip3Prime <-pmax(0,as.numeric(gsub('.*\\D(\\d*)[S]$','\\1',GenomicAlignments::cigar(reads))), na.rm=T)
-            hardClip5Prime <-pmax(0,as.numeric(gsub('^(\\d*)[H].*','\\1',GenomicAlignments::cigar(reads))), na.rm=T)
-            hardClip3Prime <-pmax(0,as.numeric(gsub('.*\\D(\\d*)[H]$','\\1',GenomicAlignments::cigar(reads))), na.rm=T)
+            softClip5Prime <-pmax(0,as.numeric(gsub('^(\\d*)[S].*','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T)
+            softClip3Prime <-pmax(0,as.numeric(gsub('.*\\D(\\d*)[S]$','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T)
+            hardClip5Prime <-pmax(0,as.numeric(gsub('^(\\d*)[H].*','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T)
+            hardClip3Prime <-pmax(0,as.numeric(gsub('.*\\D(\\d*)[H]$','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T)
             mcols(readGrgList[[counter]])$clip5Prime = pmax(softClip5Prime, hardClip5Prime)
             mcols(readGrgList[[counter]])$clip3Prime = pmax(softClip3Prime, hardClip3Prime)
-            rev = as.vector(strand(reads) == '-')
-            rev2 = grepl("_-.+of", names(reads))
+            rev = as.vector(strand(alignmentInfo) == '-')
+            rev2 = grepl("_-.+of", names(alignmentInfo))
             temp = mcols(readGrgList[[counter]])$clip5Prime
             mcols(readGrgList[[counter]])$clip5Prime[rev != rev2] = mcols(readGrgList[[counter]])$clip3Prime[rev != rev2]
             mcols(readGrgList[[counter]])$clip3Prime[rev != rev2] = temp[rev != rev2]
@@ -73,8 +73,6 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE, use.n
     }
     # remove microexons of width 1bp from list
     readGrgList <- readGrgList[width(readGrgList) > 1]
-    mcols(readGrgList[[counter]])$CB
-
     if(cleanReads){
         #extract duplicated reads from flexiplex to clean
         #leave other reads alone as supplimental alignments maybe fusion transcripts
@@ -88,11 +86,11 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE, use.n
 
         df = data.frame(name = names(readGrgList), 
             clip5 = mcols(readGrgList)$clip5Prime)
-        df = df %>% mutate(id = row_number()) %>% group_by(name) %>% summarise(primary.id = id[which.mix(clip5)])
+        df = df %>% mutate(id = row_number()) %>% group_by(name) %>% summarise(primary.id = id[which.min(clip5)])
         readGrgList = unname(readGrgList[df$primary.id])
         #readGrgList = c(readGrgList.filt, unname(readGrgList.keep))
     }
-
+    if(!use.names.OG) {names(readGrgList) <- NULL }
     return(readGrgList)
 }
 
