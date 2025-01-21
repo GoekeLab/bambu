790dd175b6436a1bfa53b28802f9285f959b6434
Chen Ying <chen_ying@gis.a-star.edu.sg>
2025-01-21 15:27:17 +0800

    add check flag when read name does not contain BC and UMI information

--- R/prepareDataFromBam.R
+++ R/prepareDataFromBam.R
@@ -28,29 +28,39 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE,
     counter <- 1
     cells <- c()
     umi <- c()
-    use.names.OG = use.names
-    if(!isFALSE(demultiplexed) | cleanReads) use.names = TRUE
-    if(grepl(".[ct]sv$",demultiplexed)){
-        readMap = read.table(demultiplexed, 
-            sep = ifelse(grepl(".tsv$",demultiplexed), "\t", ","), header = FALSE)
+    use.names.OG <- use.names
+    if(!isFALSE(demultiplexed) | cleanReads) use.names <- TRUE
+    # grepl(".[ct]sv$",demultiplexed)
+    if(!is.logical(demultiplexed)){ # if demultiplexed argument is not logical value  
+        if(file.exists(demultiplexed)){ # check if file path exists, it has to be a regular delimited file, can be compressed 
+            readMap <- fread(demultiplexed,header = FALSE, data.table = FALSE) # changed function to more efficiently read in data and also correct error of "no lines available as input" for csv format input
+        }else{
+            stop("Provided barcode to map file does not exists! Please provide the correct path to demultiplex argument!")
+        }
     }
     while (isIncomplete(bf)) {
         alignmentInfo <- readGAlignments(bf, param = ScanBamParam(tag = c("BC", "UG"), 
                                          flag = scanBamFlag(isSecondaryAlignment = FALSE)), 
                                          use.names = use.names)
         readGrgList[[counter]] <-grglist(alignmentInfo)
-        if (!isFALSE(demultiplexed)){
-            if(isTRUE(demultiplexed)){
-                mcols(readGrgList[[counter]])$BC <- ifelse(!is.na(mcols(alignmentInfo)$BC), mcols(alignmentInfo)$BC, 
-                                                        gsub("(^[GACT]+(?=_)).*", '\\1', names(readGrgList[[counter]]), perl = TRUE))
+        if (!isFALSE(demultiplexed)){ # if demultiplexed is TRUE or a string path 
+            if(isTRUE(demultiplexed)){ # if demultiplexed is TRUE
+                mcols(readGrgList[[counter]])$BC <- ifelse(!is.na(mcols(alignmentInfo)$BC), 
+                                                           mcols(alignmentInfo)$BC, 
+                                                           ifelse(grepl("[GACT]_",names(readGrgList[[counter]])), # a checkpoint to see whether BC is contained in the name, with specific format BC_UMI#READNAME
+                                                        gsub("(^[GACT]+(?=_)).*", '\\1', 
+                                                        names(readGrgList[[counter]]), perl = TRUE),
+                                                        NA))
                 mcols(readGrgList[[counter]])$UMI <- ifelse(!is.na(mcols(alignmentInfo)$UG), mcols(alignmentInfo)$UG, 
-                                                        gsub(".*((?<=_)[GACT]*(?=#)).*", '\\1', names(readGrgList[[counter]]), perl = TRUE))
-            } else{
-                mcols(readGrgList[[counter]])$BC = NA
-                mcols(readGrgList[[counter]])$UMI = "NA"
-                mcols(readGrgList[[counter]])$BC = readMap[,2][match(names(readGrgList[[counter]]),readMap[,1])]
+                                                            ifelse(grepl("[GACT]#",names(readGrgList[[counter]])), # a checkpoint to see whether UMI is contained in the name, with specific format BC_UMI#READNAME
+                                                        gsub(".*((?<=_)[GACT]*(?=#)).*", '\\1', names(readGrgList[[counter]]), perl = TRUE),
+                                                        NA))
+            } else{ # if demultiplexed is a string path
+                mcols(readGrgList[[counter]])$BC <- NA
+                mcols(readGrgList[[counter]])$UMI <- "NA"
+                mcols(readGrgList[[counter]])$BC <- readMap[,2][match(names(readGrgList[[counter]]),readMap[,1])]
                 if(ncol(readMap)>2){
-                    mcols(readGrgList[[counter]])$UMI = readMap[,3][match(names(readGrgList[[counter]]),readMap[,1])]
+                    mcols(readGrgList[[counter]])$UMI <- readMap[,3][match(names(readGrgList[[counter]]),readMap[,1])]
                 }
             }
             cells <- unique(c(cells, mcols(readGrgList[[counter]])$BC))
@@ -59,17 +69,21 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE,
             mcols(readGrgList[[counter]])$UMI <- factor(mcols(readGrgList[[counter]])$UMI, levels = umi)
         }
         if(cleanReads){
-            softClip5Prime <-suppressWarnings(pmax(0,as.numeric(gsub('^(\\d*)[S].*','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T))
-            softClip3Prime <-suppressWarnings(pmax(0,as.numeric(gsub('.*\\D(\\d*)[S]$','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T))
-            hardClip5Prime <-suppressWarnings(pmax(0,as.numeric(gsub('^(\\d*)[H].*','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T))
-            hardClip3Prime <-suppressWarnings(pmax(0,as.numeric(gsub('.*\\D(\\d*)[H]$','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T))
-            mcols(readGrgList[[counter]])$clip5Prime = pmax(softClip5Prime, hardClip5Prime)
-            mcols(readGrgList[[counter]])$clip3Prime = pmax(softClip3Prime, hardClip3Prime)
-            rev = as.vector(strand(alignmentInfo) == '-')
-            rev2 = grepl("_-.+of", names(alignmentInfo))
-            temp = mcols(readGrgList[[counter]])$clip5Prime
-            mcols(readGrgList[[counter]])$clip5Prime[rev != rev2] = mcols(readGrgList[[counter]])$clip3Prime[rev != rev2]
-            mcols(readGrgList[[counter]])$clip3Prime[rev != rev2] = temp[rev != rev2]
+            softClip5Prime <- clipFunction(cigarData = GenomicAlignments::cigar(alignmentInfo), grep_pattern = '^(\\d*)[S].*', replace_pattern = '\\1')
+            softClip3Prime <- clipFunction(cigarData = GenomicAlignments::cigar(alignmentInfo), grep_pattern = '.*\\D(\\d*)[S]$', replace_pattern = '\\1')
+            hardClip5Prime <- clipFunction(alignData = GenomicAlignments::cigar(alignmentInfo), grep_pattern = '^(\\d*)[H].*', replace_pattern = '\\1')
+            hardClip3Prime <- clipFunction(alignData = GenomicAlignments::cigar(alignmentInfo), grep_pattern = '.*\\D(\\d*)[H]$', replace_pattern = '\\1')
+            # softClip5Prime <-suppressWarnings(pmax(0,as.numeric(gsub('^(\\d*)[S].*','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T))
+            # softClip3Prime <-suppressWarnings(pmax(0,as.numeric(gsub('.*\\D(\\d*)[S]$','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T))
+            # hardClip5Prime <-suppressWarnings(pmax(0,as.numeric(gsub('^(\\d*)[H].*','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T))
+            # hardClip3Prime <-suppressWarnings(pmax(0,as.numeric(gsub('.*\\D(\\d*)[H]$','\\1',GenomicAlignments::cigar(alignmentInfo))), na.rm=T))
+            mcols(readGrgList[[counter]])$clip5Prime <- pmax(softClip5Prime, hardClip5Prime)
+            mcols(readGrgList[[counter]])$clip3Prime <- pmax(softClip3Prime, hardClip3Prime)
+            rev <- as.vector(strand(alignmentInfo) == '-')
+            rev2 <- grepl("_-.+of", names(alignmentInfo))
+            temp <- mcols(readGrgList[[counter]])$clip5Prime
+            mcols(readGrgList[[counter]])$clip5Prime[rev != rev2] <- mcols(readGrgList[[counter]])$clip3Prime[rev != rev2]
+            mcols(readGrgList[[counter]])$clip3Prime[rev != rev2] <- temp[rev != rev2]
         }
         
         counter <- counter + 1
@@ -84,10 +98,10 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE,
     }
     # remove microexons of width 1bp from list
     readGrgList <- readGrgList[width(readGrgList) > 1]
-    numNoBCs = sum(is.na(mcols(readGrgList)$BC))
+    numNoBCs <- sum(is.na(mcols(readGrgList)$BC))
     if(numNoBCs > 0){
         message("Removing ", numNoBCs, " reads that were not assigned barcodes. If this is unexpected check the barcode map input")
-    readGrgList = readGrgList[!is.na(mcols(readGrgList)$BC)]
+        readGrgList <- readGrgList[!is.na(mcols(readGrgList)$BC)]
     }
     if(cleanReads){
         #extract duplicated reads from flexiplex to clean
@@ -102,10 +116,10 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE,
 
        #select alignments closest to barcode
         start.ptm <- proc.time()
-        df = data.frame(name = names(readGrgList), 
+        df <- data.frame(name = names(readGrgList), 
             clip5 = mcols(readGrgList)$clip5Prime)
-        df = df %>% mutate(id = row_number()) %>% group_by(name) %>% summarise(primary.id = id[which.min(clip5)])
-        readGrgList = unname(readGrgList[df$primary.id])
+        df <- df %>% mutate(id = row_number()) %>% group_by(name) %>% summarise(primary.id = id[which.min(clip5)])
+        readGrgList <- unname(readGrgList[df$primary.id])
         end.ptm <- proc.time()
         message("Primary alignment selection time ", round((end.ptm - start.ptm)[3] / 60, 3), " mins.")
         
@@ -113,13 +127,13 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE,
     if(dedupUMI){
         #UMI deduplication by barcode
         start.ptm <- proc.time()
-        numUMIs = length(unique(mcols(readGrgList)$UMI))
+        numUMIs <- length(na.omit(unique(mcols(readGrgList)$UMI))) # remove NA UMIs
         if(numUMIs > 100){
-            df = data.frame(umi = mcols(readGrgList)$BC, 
+            df <- data.frame(umi = mcols(readGrgList)$BC, 
                 barcode = mcols(readGrgList)$UMI,
                 lengths = sum(width(readGrgList)))
-            df = df %>% mutate(id = row_number()) %>% group_by(barcode, umi) %>% summarise(primary.id = id[which.max(lengths)])
-            readGrgList = readGrgList[df$primary.id]
+            df <- df %>% mutate(id = row_number()) %>% group_by(barcode, umi) %>% summarise(primary.id = id[which.max(lengths)])
+            readGrgList <- readGrgList[df$primary.id]
             end.ptm <- proc.time()
             message("UMI deduplication time ", round((end.ptm - start.ptm)[3] / 60, 3), " mins.")
         } else {
@@ -129,8 +143,15 @@ prepareDataFromBam <- function(bamFile, yieldSize = NULL, verbose = FALSE,
  
         #readGrgList = c(readGrgList.filt, unname(readGrgList.keep))
     }
-    if(!use.names.OG) {names(readGrgList) <- NULL }
-    seqlevels(readGrgList)=as.character(unique(getChrFromGrList(readGrgList)))
+    if(!use.names.OG) names(readGrgList) <- NULL
+    seqlevels(readGrgList) <- as.character(unique(getChrFromGrList(readGrgList)))
     return(readGrgList)
 }
 
+
+#' Function to clip sequences 
+#' @noRd
+clipFunction <- function(cigarData, grep_pattern, replace_pattern){
+    return(suppressWarnings(pmax(0,as.numeric(gsub(grep_pattern,replace_pattern,
+                                                   cigarData)), na.rm=T)))
+}
