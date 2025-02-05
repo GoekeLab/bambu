b122513e4ce1c15c51d594bbc19b0a906edea87c
lingminhao <lingminhao31@gmail.com>
2025-02-05 17:03:26 +0800

    fix bug about CompressedCharacterList

--- R/bambu.R
+++ R/bambu.R
@@ -268,7 +268,7 @@ bambu <- function(reads, annotations = NULL, genome = NULL, NDR = NULL,
             #load in the barcode clustering from file if provided
             iter <- seq_len(ncol(metadata(quantData_i)$countMatrix)) # iter is integer
             if(!is.null(clusters)){
-                if(class(clusters)!="CompressedCharacterList"){ # !is.list(clusters) is FALSE for CompressedCharacterList 
+                if(class(clusters[[i]])!="CompressedCharacterList"){ # !is.list(clusters) is FALSE for CompressedCharacterList 
                     clusterMaps <- NULL
                     for(j in seq_along(metadata(quantData_i)$sampleNames)){ #load in a file per sample name provided
                         clusterMap <- fread(clusters[[j]], header = FALSE, 
