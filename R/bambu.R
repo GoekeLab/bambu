7de4994aeca0b077ee74520cc6efb57752aa6df9
suiyue <suiyue@gis.a-star.edu.sg>
2025-02-11 15:41:15 +0800

    remove checking demultiplexed

--- R/bambu.R
+++ R/bambu.R
@@ -149,8 +149,7 @@ bambu <- function(reads, annotations = NULL, genome = NULL, NDR = NULL,
             processByBam <- TRUE
         }
         if(mode == "multiplexed"){
-            if(!(demultiplexed)) 
-                demultiplexed <- TRUE
+            demultiplexed <- TRUE
             cleanReads <- TRUE
             opt.em <- list(degradationBias = FALSE)
             quant <- FALSE
