1fccbe126b5c003d67e8b98bfa3f47288abcf50c
SuiYue-2308 <suiyue@gis.a-star.edu.cn>
2025-02-27 09:52:48 +0800

    fix problem that when speficying readClass.outputDir se returns to the path, not the read class

--- R/bambu-processReads.R
+++ R/bambu-processReads.R
@@ -110,7 +110,6 @@ bambu.processReads <- function(reads, annotations, genomeSequence,
                 readClass.outputDir, ask = FALSE),
                 paste0(readClassFile,"_readClassSe"), ext = ".rds")
             saveRDS(readClassList[[i]], file = readClassFile)
-            readClassList[[i]] <- readClassFile
         }
     }
     #TODO don't output list, current there because discovery needs it
