44fd16da22bc3065a01967acbc5ffa2f5126c8f1
Chen Ying <chen_ying@gis.a-star.edu.sg>
2026-01-06 10:24:21 +0800

    update xgb.save binary files and internal models

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,7 +4,7 @@
 # fitXGBoostModel() in test_xgboost.R 
 data_train <- matrix(seq(1:300000), nrow=100000)
 data_test <- matrix(c(seq(1:28000), seq(280001:300000)), nrow=16000)
-labels_train <- c(rep(1,50000), rep(0,50000))
+labels_train <- as.logical(c(rep(1,50000), rep(0,50000)))
 xgb_model <- fitXGBoostModel(labels_train, data_train, show.cv=TRUE)
 # Extract the predictions and results from the list
 xgb_predictions = predict(xgb_model, data_test)
@@ -112,9 +112,13 @@ defaultModels$transcriptModelSE = NULL
 #saveRDS(defaultModels, "./inst/extdata/defaultModels.rds")
 defaultModels = readRDS(system.file("extdata", "defaultModels.rds",
                                     package = "bambu"))
+
+
+
 defaultModels$transcriptModelME = xgb.load("./inst/extdata/read_class_ME.model")
 defaultModels$transcriptModelSE = xgb.load("./inst/extdata/read_class_SE.model")                                    
-
+xgb.save(defaultModels$transcriptModelME, "./inst/extdata/read_class_ME.ubj")
+xgb.save(defaultModels$transcriptModelSE, "./inst/extdata/read_class_SE.ubj")
 # How to get pre trained junction model standardJunctionModels_temp
 # added "saveRDS(junctionModel, "./inst/extdata/standardJunctionModels_temp.txt")" to junctionErrorCorrection
 # ran Bambu with GNex_HepG2_directRNA_replicate5_run1_genome
